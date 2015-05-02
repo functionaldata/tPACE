0a38e6314ec8b230b6aba2d5e12a270d73b79b43
Pantelis <p.z.hadjipantelis@gmail.com>
2015-05-01 18:53:18 -0700

    Building up mapX1d function and tests

--- tests/test_IsRegular.R
+++ tests/test_IsRegular.R
@@ -5,3 +5,4 @@ test_that("basic valid lists arguments do not return any errors ", {
   expect_equal(IsRegular(list(c(1,2,3  ), c(1,2,3,4), c(1,2,3,4))), 1) 
   expect_equal(IsRegular(list(c(1,2   ), c(1,2,3  ), c(1,2,3,4))), 0) 
 }
+)
