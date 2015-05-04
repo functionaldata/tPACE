8b9dc3748402cd2671915856cbd17f7dae24c806
Xiongtao Dai <dai@ucdavis.edu>
2015-05-04 16:28:49 -0700

    lwls1d

--- lwls1d.R
+++ lwls1d.R
@@ -2,15 +2,15 @@
 # nwe argument (in the Matlab version) is merged into kern.
 # bw: bandwidth, a scalar.
 # kern: kernel used: 'gauss', 'rect', 'tria', 'epan' (default), 'quar', 'trwt', or 'tcub'. (See \url{http://en.wikipedia.org/wiki/Kernel_%28statistics%29#In_non-parametric_statistics})
-# npoly: degree of local polynomial; 2 corresponds to local linear.
+# npoly: degree of local polynomial; 1 corresponds to local linear.
 # nder: order of derivative.
 # xin: a vector of x-coordinate.
 # yin: a vector of y-coordinate.
 # win: a vector of weights on the observations.
 # xout: a vector of output x-coordinate grid.
-# Returns: the fitted locfit object for the convenience of future operations such as gcv. Use predict(fit) to obtain the fitted values.
+# returnFit: If TRUE, return the fitted locfit object for the convenience of future operations such as gcv. Use predict(fit) to obtain the fitted values.
 # Difference from lwls.m: There is no nwe argument. The kernels supported are different. No local bandwidth choice is supported as it is never actually called in Matlab PACE.
-lwls1d <- function(bw, kern='epan', npoly=1, nder=0, xin, yin, win=NULL, xout=xin) {
+lwls1d <- function(bw, kern='epan', npoly=1, nder=0, xin, yin, win=NULL, xout=dat(), returnFit=FALSE) {
     if (npoly < nder)
         stop('Degree of Polynomial should be no less than the order of derivative')
 
@@ -18,8 +18,6 @@ lwls1d <- function(bw, kern='epan', npoly=1, nder=0, xin, yin, win=NULL, xout=xi
         stop('Bandwidth choice must be positive')
 
     datin <- data.frame(x=as.numeric(xin), y=as.numeric(yin))
-# check the bandwidth specification!
-    form <- as.formula(sprintf('y ~ lp(x, h=%g, deg=%d)', bw, npoly + 1))
     
     if (nder == 0L)
         nder <- numeric(0) # as in locfit
@@ -29,11 +27,17 @@ lwls1d <- function(bw, kern='epan', npoly=1, nder=0, xin, yin, win=NULL, xout=xi
     if (missing(win))
         win <- rep(1, length(xin))
 
-    if (missing(xout))
-        fit <- locfit(form, datin, weights=win)
-    else
-        fit <- locfit(form, datin, weights=win, deriv=nder, ev=xout)
+    fit <- locfit(y ~ lp(x, h=bw, deg=npoly), datin, weights=win, deriv=nder, ev=xout, kern=kern)
 
-    return(fit)
+    if (returnFit)
+        return(fit)
+
+    if (missing(xout)) {
+        ret <- fitted(fit)
+    } else {
+        ret <- predict(fit)
+    }
+
+    return(ret)
 }
 
--- tests/lwls.R
+++ tests/lwls.R
@@ -4,12 +4,68 @@ x <- seq(0, pi, by=0.1)
 y <- sin(x) + rnorm(length(x), sd=0.2)
 plot(x, y)
 # To MATLAB
-write.table(format(data.frame(x=x, y=y), digits=16), file='lwls1.csv', sep=',', row.names=FALSE, col.names=FALSE, quote=FALSE)
+# write.table(format(data.frame(x=x, y=y), digits=16), file='lwls1.csv', sep=',', row.names=FALSE, col.names=FALSE, quote=FALSE)
+## Matlab code
+# addpath('~/PACE_matlab/release2.17/PACE')
+# dat = csvread('lwls1.csv');
+# [~, val] = lwls(0.3, 'epan', 0, 1, 0, dat(:, 1)', dat(:, 2), ones(1, size(dat, 1)), [0, 0.15, 0.35, 0.55, 3], 0)
 
-tmp <- lwls1d(0.3, 'epan', xin=x, yin=y)
-plot(tmp)
-tmp <- lwls1d(0.3, 'epan', xin=x, yin=y, xout=c(0, 0.15, 0.35, 0.55, 3.14))
-predict(tmp)
+# tmp <- lwls1d(0.3, 'epan', xin=x, yin=y)
+# plot(tmp)
 
-tmp1 <- locfit(y ~ lp(x, h=0.1), data.frame(x, y))
-plot(tmp1)
+
+test_that('Epanechnikov kernel matches', {
+# degree 1
+    expect_equal(lwls1d(0.3, 'epan', xin=x, yin=y, xout=c(0, 0.15, 0.35, 0.55, 3)), 
+                 c(-0.075034417378891, 0.142526970448573, 0.380805485912617,   0.573996912896636, 0.232449699086771))
+# degree 2
+    expect_equal(lwls1d(0.3, 'epan', npoly=2, xin=x, yin=y, xout=c(0, 0.15, 0.35, 0.55, 3)), 
+                 c(-0.125290762148466, 0.130962171169946, 0.442387233004433, 0.496519823276647, 0.346408319249383))
+})
+
+
+test_that('bandwidth works', {
+    expect_equal(lwls1d(0.5, 'epan', xin=x, yin=y, xout=c(0, 0.15, 0.35, 0.55, 3)), 
+                 c(-0.114913056431249,  0.132079727835864,   0.365292140767686, 0.569502194595946,   0.218632400355849))
+    expect_equal(lwls1d(0.1, 'epan', xin=x, yin=y, xout=c(0, 0.15, 0.35, 0.55, 3)), 
+                 c(-0.125290762148466,  0.084052844902148,   0.534948131880310, 0.488730072830666,   0.412855918365678))
+})
+
+
+##  Normal kernel DO NOT MATCH
+# test_that('Normal kernel matches', {
+    # expect_equal(lwls1d(0.3, 'gauss', xin=x, yin=y, xout=c(0, 0.15, 0.35, 0.55, 3)), 
+                 # c(-0.062129832105935, 0.134704121324048,   0.361071058656017,   0.553453878429684, 0.207951702919279))
+# })
+
+
+test_that('Rectangular kernel matches', {
+    expect_equal(lwls1d(0.3, 'rect', xin=x, yin=y, xout=c(0.15, 0.35, 0.55, 3)), 
+                 c(0.140580458121389,   0.369243683204375,   0.611958566485952, 0.220769364459782))
+# BUT THE BOUNDARY DOES NOT MATCH
+})
+
+
+test_that('Derivative works', {
+    expect_equal(lwls1d(0.3, nder=1, xin=x, yin=y, xout=c(0, 0.15, 0.35, 0.55, 3)),
+                        c(0.985197231385895,   1.767923028863129,   0.837595175365137, 0.929221260582535,  -0.689889054107760))
+    expect_equal(lwls1d(0.3, npoly=2, nder=1, xin=x, yin=y, xout=c(0, 0.15, 0.35, 0.55, 3)),
+                 c(4.452885020486605,  1.715294894713705,   0.837595175365137, 0.929221260582535,  -1.758251118132247))
+})
+
+
+test_that('Can return fit object', {
+    expect_equal(predict(lwls1d(0.3, 'epan', xin=x, yin=y, returnFit=TRUE)), 
+                 predict(locfit(y ~ lp(x, h=0.3, deg=1), data.frame(x=x, y=y), kern='epan', ev=dat())))
+    expect_equal(predict(lwls1d(0.3, 'epan', xin=x, yin=y, xout=c(0, 1, 3), returnFit=TRUE)), 
+                 predict(locfit(y ~ lp(x, h=0.3, deg=1), data.frame(x=x, y=y), kern='epan', ev=c(0, 1, 3))))
+})
+
+
+test_that('weight works', {
+    win <- matrix(1, nrow(datin))
+    win[1:3] <- 2
+    expect_equal(lwls1d(0.3, 'epan', xin=x, yin=y, win=win, xout=c(0, 0.15, 0.35, 0.55, 3)), 
+                 c(-0.075034417378891,  0.123584297605571,   0.353323309535711, 0.573996912896636,   0.232449699086771))
+
+})
