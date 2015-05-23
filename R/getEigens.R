83d2b382cffd8bfc461a0d8c4e6793c9dd5f39b8
Pantelis <p.z.hadjipantelis@gmail.com>
2015-05-22 17:07:32 -0700

    Added getEigens.R and populated the DESCRIPTION a bit

---
+++ R/getEigens.R
@@ -0,0 +1,54 @@
+getEigens <- function(xcov,out1,out21,noeig, varargin){
+#   function [lambda, phi, eigen, noeig] = getEigens(xcov,out1,out21,noeig, varargin)
+
+ 
+
+  h=diff(range(out21))/(length(out21)-1);
+
+  numGrids = nrow(xcov)
+  #opts.v0 = t(seq(0.1,0.9,length.out = numGrids))
+  eigsOutput = eigs(xcov, k = numGrids-2, which = "LR")
+  d = eigsOutput$values
+  eigenV = eigsOutput$vectors
+  # at most ngrid-2 eigenvalues can be obtained for nonsymmetric or complex problems
+  # "LM" corresponds to Largest Magnitude, another option may be "LR": Largest Real part
+  
+  idx = which(is.complex(d)) # to remove any imaginary eigenvalues
+  if(invalid(idx) == FALSE){ # if there are any imaginary eigenvalues
+    stop(sprintf("%d eigenvalues are complex. The estimated auto-covairance surface is not symmetric!",length(idx)))
+  }
+  
+  idx = which(d <= 0) # to remove nonpositive eigenvalues
+  if(invalid(idx) == FALSE){ # if there are any nonpositive eigenvalues
+    warning(sprintf("Warning: %d real eigenvalues are negative or zero and are removed!",length(idx)))
+    d = d[d > 0]
+    eigenV = eigenV[,d>0]
+  }
+    
+  if (noeig > length(d)){
+    noeig = length(d);
+    warning(sprintf("Warning: at most %d number of PC can be selected!", (noeig)  ))  
+  }
+
+  eigenV = eigenV[,1:noeig];
+  d = d[1:noeig];
+
+  eigenV = eigenV/sqrt(h);
+  lambda = h * d;
+
+  # normalized eigen functions
+  eigenV = apply(eigenV,2, function(x) x / sqrt(trapz(out21,(x)^2)) )
+  eigenV = apply(eigenV,2, function(x) if (x[1] <= x[2]){ x }else{ -x} )
+  
+  # interpolate from the normalized eigenfunctions
+  phi = apply(eigenV,2, function(x) interp1(x= out21, y= x, xi=out1, method="spline"))
+  if (noeig ==1){
+    phi = t(phi)
+  }
+  
+  # normalized smoothed eigenfunctions
+  phi  = apply(phi,2, function(x) x / sqrt(trapz(out1,(x)^2)) )
+
+  return( list(lambda = lambda, phi = phi, eigen = eigenV, noeig = noeig) )
+}
+
