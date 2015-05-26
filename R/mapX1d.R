ae0baf76d8f3273356fd82f534156159c9a28b2b
Xiongtao <dai@ucdavis.edu>
2015-05-26 01:42:51 +0100

    changed desc, added namespace

--- R/mapX1d.R
+++ R/mapX1d.R
@@ -3,14 +3,26 @@
 # x, y : vectors of 1 * n
 # newx : vector of 1 * m
 # newy : vector of 1*m
-mapX1d <- function(x,y,newx){
+# mapX1d <- function(x,y,newx){
   
-  if( is.vector(y) ){
-    return(y[is.element(x,newx)])
-  }else if(is.matrix(y)){
-    return(y[is.element(x,newx),])
-  }else{
-    warning('y cannot be empty!\n')
-    return(NaN)
-  }   
+  # if( is.vector(y) ){
+    # return(y[is.element(x,newx)])
+  # }else if(is.matrix(y)){
+    # return(y[is.element(x,newx),])
+  # }else{
+    # warning('y cannot be empty!\n')
+    # return(NaN)
+  # }   
+# }
+
+mapX1d <- function(x, y, newx) {
+    if (!all(newx %in% x)) 
+        warning('Interpolation occured: you might want to increase the out1 coverage')
+        
+    if (min(newx) + 100 * .Machine$double.eps < min(x) || max(newx) > max(x) + 100 * .Machine$double.eps)
+        warning('Extrapolation occured')
+
+    newy <- approxExtrap(x, y, newx, method='linear')$y
+    
+    return(newy)
 }
