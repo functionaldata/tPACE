4ae1cb16f5ad1189f1c91525478ff3b9348e1295
hadjipantelis <p.z.hadjipantelis@gmail.com>
2015-05-30 23:45:50 -0700

    Remove ..Rcheck folder

--- ..Rcheck/tests/test_IsRegular.R
+++
@@ -1,8 +0,0 @@
-cat("\ntests for 'IsRegular'")
-
-test_that("basic valid lists arguments do not return any errors ", { 
-  expect_equal(IsRegular(list(c(1,2,3,4), c(1,2,3,4), c(1,2,3,4))), 'Dense') 
-  expect_equal(IsRegular(list(c(1,2,3  ), c(1,2,3,4), c(1,2,3,4))), 'RegularWithMV') 
-  expect_equal(IsRegular(list(c(1,2   ), c(1,2,3  ), c(1,2,3,4))), 'Sparse') 
-}
-)
