3ecdce2b9af1c5e31faf042c8a38f6326b5afd9d
hadjipantelis <p.z.hadjipantelis@gmail.com>
2015-05-31 00:09:55 -0700

    Changing SetOptions.R

--- R/SetOptions.R
+++ R/SetOptions.R
@@ -34,23 +34,22 @@
 
 SetOptions = function(y, t, p){ 
 
-  bwmu = p$bwmu;                bwmu_gcv = p$bwmu_gcv; 
-  bwxcov = p$bwxcov;            bwxcov_gcv = p$bwxcov_gcv;
-  ntest1 = p$ntest1;            ngrid1 = p$ngrid1; 
-  selection_k = p$selection_k;  FVE_threshold = p$FVE_threshold;
-  maxk = p$maxk;                
-  regular = p$regular;          error = p$error; 
-  ngrid = p$ngrid;              method = p$method; 
-  shrink = p$shrink;            newdata = p$newdata; 
-  kernel = p$kernel;            numBins = p$numBins; 
-  yname = p$yname;              screePlot = p$screePlot; 
-  designPlot = p$designPlot;    rho = p$rho;
-  verbose = p$verbose;          corrPlot = p$corrPlot;
-  xmu = p$xmu;                  method_mu = p$method_mu;
-  out_percent = p$out_percent;  xcov = p$xcov
-                                use_binned_data = p$use_binned_data;
+  bwmu =p[['bwmu']];                bwmu_gcv =p[['bwmu_gcv']]; 
+  bwxcov =p[['bwxcov']];            bwxcov_gcv =p[['bwxcov_gcv']];
+  ntest1 =p[['ntest1']];            ngrid1 =p[['ngrid1']]; 
+  selection_k =p[['selection_k']];  FVE_threshold =p[['FVE_threshold']];
+  maxk =p[['maxk']];                
+  regular =p[['regular']];          error =p[['error']];
+  ngrid =p[['ngrid']];              method =p[['method']];
+  shrink =p[['shrink']];            newdata =p[['newdata']] ;
+  kernel =p[['kernel']];            numBins =p[['numBins']];
+  yname =p[['yname']];              screePlot =p[['screePlot']];
+  designPlot =p[['designPlot']];    rho =p[['rho']];
+  verbose =p[['verbose']];          corrPlot =p[['corrPlot']];
+  xmu =p[['xmu']];                  method_mu =p[['method_mu']];
+  out_percent =p[['out_percent']];  xcov =p[['xcov']];
+                                    use_binned_data =p[['use_binned_data']];
 
-  
   if(is.null(bwmu)){ # bandwidth choice for mean function is using CV or GCV
     bwmu = 0;   
   }
