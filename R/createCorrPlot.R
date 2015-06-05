fd87c77d1ceac4fada515479bc4e8e0a323e273e
dmchris <haoji@ucdavis.edu>
2015-06-04 23:00:39 -0700

    modify createCorrPlot.R & include library plot3D and rgl for 3d plotting

--- R/createCorrPlot.R
+++ R/createCorrPlot.R
@@ -4,20 +4,52 @@
 # Input
 ######
 #  yy : returned object from FPCA().
+#  corrPlotType: 'Smoothed': plot the smoothed cov surface 
+#                'Fitted': plot the fitted cov surface
+#  isInteractive: TRUE: interactive plot
+#                 FALSE: printable plot
+#  ...: passed into persp3d or persp3D for plotting options
 
-createCorrPlot = function(yy){
-  no_opt = length(yy$eigVal)
-  if(no_opt == NULL){
-    warning('Warning: Input is not a valid FPCA or FPCder output.')
-  } else if(no_opt == 1){
-    warning('Warning: Correlation surface is not available when only one principal component is used.')
-  } else {
-    yname = yy$yname
-    regGrid = yy$regGrid
-    xcorr = yy$xcorr
-    
-    plot3D(regGrid, regGrid, xcorr,
-      xlab = 't', ylab = 't', zlab = 'Correlation Surface',
-      main = paste('Fitted correlation surface for function', yname))
+createCorrPlot = function(yy, corrPlotType = 'Fitted', isInteractive = FALSE){
+  # Check if plotting covariance surface for fitted covariance surface is proper
+  if(corrPlotType == 'Fitted'){
+    no_opt = length(yy$eigVal)
+    if(no_opt == NULL){
+      warning('Warning: Input is not a valid FPCA or FPCder output.')
+      return()
+    } else if(no_opt == 1){
+      warning('Warning: Correlation surface is not available when only one principal component is used.')
+      return()
+    }
   }
+  
+  yname = yy$yname
+  regGrid = yy$regGrid
+  truncGrid = yy$truncatedRegGrid
+  smoothCov = yy$smoothedCov
+  fitCov = yy$fittedCov
+  
+  if(isInteractive){ # Interactive Plot
+    if(corrPlotType == 'Fitted'){
+      persp3d(truncGrid, truncGrid, fitCov, col = 'blue',
+        xlab = 't', ylab = 't', zlab = 'Fitted Correlation Surface',
+        main = paste('Fitted correlation surface for function', yname))
+    } else if(corrPlotType == 'Smoothed'){
+      persp3d(regGrid, regGrid, smoothCov, col = 'blue',
+        xlab = 't', ylab = 't', zlab = 'Smoothed Correlation Surface',
+        main = paste('Smoothed correlation surface for function', yname))
+
+    }
+  } else { # Static plot
+    if(corrPlotType == 'Fitted'){
+      persp3D(truncGrid, truncGrid, fitCov,
+        xlab = 't', ylab = 't', zlab = 'Fitted Correlation Surface',
+        main = paste('Fitted correlation surface for function', yname))
+    } else if(corrPlotType == 'Smoothed'){
+      persp3D(regGrid, regGrid, smoothCov,
+        xlab = 't', ylab = 't', zlab = 'Smoothed Correlation Surface',
+        main = paste('Smoothed correlation surface for function', yname))      
+    }
+  }
+
 }
