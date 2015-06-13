a6d4012975ae4b2ea120fd575c74eec1a279c9d3
hadjipantelis <p.z.hadjipantelis@gmail.com>
2015-06-13 03:31:23 -0700

    Some ident. fixes and comments in Rmullwlsk

--- misc/Rmullwlsk.cpp
+++ misc/Rmullwlsk.cpp
@@ -1,9 +1,8 @@
 #include <RcppEigen.h>
 #include <map>          // to map kernels to integers for the switch
 #include <string>       // to read in the kernel name
-#include <vector>       // to read in the kernel name
+#include <vector>       // to use vectors
 #include <algorithm>    // to get the intersect and sort
-#include <numeric>
 
 // [[Rcpp::depends(RcppEigen)]]
 // [[Rcpp::export]]
@@ -47,18 +46,19 @@ Eigen::MatrixXd Rmullwlsk( const Eigen::Map<Eigen::VectorXd> & bw, const std::st
         std::vector <unsigned int> list1, list2; 
         for (unsigned int y = 0; y != tPairs.cols(); y++){ 
           if ( (tPairs(0,y) >= (xgrid(j) -(bw(0)-pow(10,-6))) ) && (tPairs(0,y) <= (xgrid(j) + (bw(0)+ pow(10,-6)))) ) {
-    list1.push_back(y);
+            list1.push_back(y);
           }         
           if ( (tPairs(1,y) >= (ygrid(i) -(bw(1)-pow(10,-6))) ) && (tPairs(1,y) <= (ygrid(i) + (bw(1)+ pow(10,-6)))) ) {
             list2.push_back(y);
           }
         }
 
-        //get intersection 
+        //get intersection between the two lists 
         std::set_intersection(list1.begin(), list1.begin() + list1.size(), list2.begin(), list2.begin() + list2.size(), std::back_inserter(indx));     
-
-     list2.clear();
-     list1.clear();
+        
+        // Maybe be redundant  
+        list2.clear();
+        list1.clear();
 
       } else{ // just get the whole deal
         for (unsigned int y = 0; y != tPairs.cols(); ++y){
@@ -67,7 +67,7 @@ Eigen::MatrixXd Rmullwlsk( const Eigen::Map<Eigen::VectorXd> & bw, const std::st
       }  
 
       unsigned int indxSize = indx.size();
-     indx.clear();
+      indx.clear(); // Again this clear maybe redundant / check in future
       Eigen::VectorXd lw(indxSize);  
       Eigen::VectorXd ly(indxSize);
       Eigen::MatrixXd lx(2,indxSize);
@@ -106,7 +106,7 @@ Eigen::MatrixXd Rmullwlsk( const Eigen::Map<Eigen::VectorXd> & bw, const std::st
           case 2 : // Rect
             temp=(lw.array())*.25 ; //this might be wrong.
             break;
-          case 3 : // Gauss
+          case 3 : // Gauss //this and the following kernels are not tested. DO NOT USE!
             temp =  ((-.5*(llx.row(1).array().pow(2))).exp())/(sqrt(2.*M_PI))  *   
               ((-.5*(llx.row(0).array().pow(2))).exp())/(sqrt(2.*M_PI))  *
               (lw.array()); 
@@ -119,24 +119,23 @@ Eigen::MatrixXd Rmullwlsk( const Eigen::Map<Eigen::VectorXd> & bw, const std::st
             break;
           case 5 :  // Quar
             temp =  (lw.array()) * ((1-llx.row(0).array()).array().pow(2)) *
-               ((1-llx.row(1).array()).array().pow(2))*(225./256.);
+              ((1-llx.row(1).array()).array().pow(2))*(225./256.);
             break;
-         } 
+        } 
 
-         // make the design matrix
-         Eigen::MatrixXd X(indxSize ,3);
-         X.setOnes();    
-         X.col(1) = lx.row(0).array() - xgrid(j);
-         X.col(2) = lx.row(1).array() - ygrid(i); 
-         Eigen::LLT<Eigen::MatrixXd> llt_XTWX(X.transpose() * temp.asDiagonal() *X);
-         Eigen::VectorXd beta = llt_XTWX.solve(X.transpose() * temp.asDiagonal() * ly);
-         mu(i,j)=beta(0); 
-       } else {
-         Rcpp::Rcout <<"No enough points in local window, please increase bandwidth." << std::endl;  
-         Rcpp::Rcout <<"The meter value is:" << meter << std::endl;  
-         return (tPairs);
-       }
- 
+        // make the design matrix
+        Eigen::MatrixXd X(indxSize ,3);
+        X.setOnes();    
+        X.col(1) = lx.row(0).array() - xgrid(j);
+        X.col(2) = lx.row(1).array() - ygrid(i); 
+        Eigen::LLT<Eigen::MatrixXd> llt_XTWX(X.transpose() * temp.asDiagonal() *X);
+        Eigen::VectorXd beta = llt_XTWX.solve(X.transpose() * temp.asDiagonal() * ly);
+        mu(i,j)=beta(0); 
+      } else {
+        Rcpp::Rcout <<"No enough points in local window, please increase bandwidth." << std::endl;  
+        Rcpp::Rcout <<"The meter value is:" << meter << std::endl;  
+        return (tPairs);
+      }
     }
   }
  
@@ -147,10 +146,9 @@ Eigen::MatrixXd Rmullwlsk( const Eigen::Map<Eigen::VectorXd> & bw, const std::st
 
   Eigen::MatrixXd result(ygrid.size(),xgrid.size());
   result = mu + mu.transpose();
-  result.diagonal() = 0.5 *   result.diagonal();
-
-  //return ( BSold * Zst.transpose() * llt_VY.solve(yf) );    
-return ( result ); 
+  result.diagonal() = 0.5 * result.diagonal();
+      
+  return ( result ); 
 }
 
  
