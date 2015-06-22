22d49ed11042b5f989defcf144bf0ff7a1d50ff7
Xiongtao Dai <dai@ucdavis.edu>
2015-06-22 10:44:24 -0700

    rotate V2

---
+++ R/rotateLwls2dV2.R
@@ -0,0 +1,18 @@
+# Rotate the data and then smooth the diagonal elements. We use quadratic terms on either direction, rather than only orthogonal to the diagonal.
+# xout: a matrix of two columns containing the diagonal elements.
+
+rotateLwls2dV2 <- function(bw, kern='epan', xin, yin, win=NULL, xout) {
+  
+  if (length(bw) == 1)
+    bw <- c(bw, bw)
+    
+  if (missing(win) || is.null(win))
+    win <- rep(1, nrow(datin))
+    
+  fit <- Rrotatedmullwlsk(bw, kern, t(xin), yin, win, t(xout), npoly=1, bwCheck=FALSE)
+  
+  if (any(is.nan(fit)))
+    stop('Something wrong with the rotate smoothed results')
+    
+  return(fit)
+}
