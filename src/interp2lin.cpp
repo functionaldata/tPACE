8c6ea8b6966d48f59029744a121b00b077b87565
hadjipantelis <p.z.hadjipantelis@gmail.com>
2015-06-30 17:14:49 -0700

    Changed interp2lin out-of-grid behaviour (NaN instead of ::error

--- src/interp2lin.cpp
+++ src/interp2lin.cpp
@@ -1,6 +1,7 @@
 #include <RcppEigen.h>
 #include <algorithm>    // to get std::lower_bound
 #include <iterator>     // to get std::iterator
+#include <limits>       // to get NaN
 
 // [[Rcpp::depends(RcppEigen)]]
 // [[Rcpp::export]]
@@ -24,7 +25,8 @@ Eigen::VectorXd interp2lin( const Eigen::Map<Eigen::VectorXd> & xin, const Eigen
     Rcpp::stop("Input Z-grid does not have the same number of points as the product of #Input Y-grid times #Input X-grid.");
   } else if ( nUnknownPoints != you.size() ){
     Rcpp::stop("Output Y-grid does not have the same number of points as Output X-grid.");
-  } else if ( xin.minCoeff() >  xou.minCoeff() ){
+  }
+  /* else if ( xin.minCoeff() >  xou.minCoeff() ){
     Rcpp::stop("Output X-grid  is outside the lower range of the input X-grid.");
   } else if ( yin.minCoeff() >  you.minCoeff() ){
     Rcpp::stop("Output X-grid  is outside the lower range of the input X-grid.");
@@ -33,6 +35,12 @@ Eigen::VectorXd interp2lin( const Eigen::Map<Eigen::VectorXd> & xin, const Eigen
   } else if ( yin.maxCoeff() <  you.maxCoeff() ){
     Rcpp::stop("Output X-grid  is outside the upper ragne of the input X-grid.");
   } 
+  */
+ 
+  const double ymin = yin.minCoeff();
+  const double xmin = xin.minCoeff();
+  const double ymax = yin.maxCoeff();
+  const double xmax = xin.maxCoeff();
 
   // The actual interpolation routine
   // Check: https://en.wikipedia.org/wiki/Bilinear_interpolation#Alternative_algorithm
@@ -47,42 +55,43 @@ Eigen::VectorXd interp2lin( const Eigen::Map<Eigen::VectorXd> & xin, const Eigen
   A.setOnes(); 
 
   for (unsigned int u = 0; u !=nUnknownPoints; ++u){ 
-  
-    // Find the appropriate x coordinates/save them in xa (2-by-1)
-    // Get iterator pointing to the first element which is not less than xou(u)
-    const double* x1 = std::lower_bound(&xin[0], &xin[nXGrid], xou(u)); 
-    xa(1) = *x1;
-    xa(0) = *--x1;
-  
-    // I feel there is a bug around here. But I cannot smoke it out.
-  
-    // Find the appropriate y coordinates/save them in ya (2-by-1)
-    // Get iterator pointing to the first element which is not less than za(u)
-    const double* y1 = std::lower_bound(&yin[0], &yin[nYGrid], you(u)); 
-    ya(1) = *y1;
-    ya(0) = *--y1;
-  
-    za(0) =  zin( (std::find(&xin[0], &xin[nXGrid], xa(0)) -&xin[0]) * nXGrid + (std::find(&yin[0], &yin[nXGrid], ya(0)) -&yin[0]));
-    za(1) =  zin( (std::find(&xin[0], &xin[nXGrid], xa(0)) -&xin[0]) * nXGrid + (std::find(&yin[0], &yin[nXGrid], ya(1)) -&yin[0]));
-    za(2) =  zin( (std::find(&xin[0], &xin[nXGrid], xa(1)) -&xin[0]) * nXGrid + (std::find(&yin[0], &yin[nXGrid], ya(0)) -&yin[0]));
-    za(3) =  zin( (std::find(&xin[0], &xin[nXGrid], xa(1)) -&xin[0]) * nXGrid + (std::find(&yin[0], &yin[nXGrid], ya(1)) -&yin[0]));
+     
+    if ( (xmax  < xou(u))  || (ymax <  you(u)) || // If x/you(u) is above the upper values of xin/yin 
+         (xmin  > xou(u))  || (ymin >  you(u)) ){ // If x/you(u) is below the lower values of xin/yin
+      result(u) = std::numeric_limits<double>::quiet_NaN() ;
+      
+    } else {
+      // Find the appropriate x coordinates/save them in xa (2-by-1)
+      // Get iterator pointing to the first element which is not less than xou(u)
+      const double* x1 = std::lower_bound(&xin[0], &xin[nXGrid], xou(u));
+      const double* y1 = std::lower_bound(&yin[0], &yin[nYGrid], you(u));
 
-    // Column 2  
-    A(0,1) = xa(0);   A(1,1) = xa(0);
-    A(2,1) = xa(1);   A(3,1) = xa(1);
-    // Column 3
-    A(0,2) = ya(0);   A(1,2) = ya(1);
-    A(2,2) = ya(0);   A(3,2) = ya(1);
-    // Column 4
-    A(0,3) = ya(0) * xa(0);   A(1,3) = ya(1) * xa(0);
-    A(2,3) = ya(0) * xa(1);   A(3,3) = ya(1) * xa(1);
+      xa(1) = *x1;
+      ya(1) = *y1;
+      xa(0) = *--x1;
+      ya(0) = *--y1;
+
+      za(0) =  zin( (std::find(&xin[0], &xin[nXGrid], xa(0)) -&xin[0]) * nXGrid + (std::find(&yin[0], &yin[nXGrid], ya(0)) -&yin[0]));
+      za(1) =  zin( (std::find(&xin[0], &xin[nXGrid], xa(0)) -&xin[0]) * nXGrid + (std::find(&yin[0], &yin[nXGrid], ya(1)) -&yin[0]));
+      za(2) =  zin( (std::find(&xin[0], &xin[nXGrid], xa(1)) -&xin[0]) * nXGrid + (std::find(&yin[0], &yin[nXGrid], ya(0)) -&yin[0]));
+      za(3) =  zin( (std::find(&xin[0], &xin[nXGrid], xa(1)) -&xin[0]) * nXGrid + (std::find(&yin[0], &yin[nXGrid], ya(1)) -&yin[0]));
+
+      // Column 2  
+      A(0,1) = xa(0);   A(1,1) = xa(0);
+      A(2,1) = xa(1);   A(3,1) = xa(1);
+      // Column 3
+      A(0,2) = ya(0);   A(1,2) = ya(1);
+      A(2,2) = ya(0);   A(3,2) = ya(1);
+      // Column 4
+      A(0,3) = ya(0) * xa(0);   A(1,3) = ya(1) * xa(0);
+      A(2,3) = ya(0) * xa(1);   A(3,3) = ya(1) * xa(1);
   
-    fq << 1 , xou(u), you(u), xou(u)*you(u);
+      fq << 1 , xou(u), you(u), xou(u)*you(u);
   
-    // Rcpp::Rcout << "xa: " << xa.transpose() << ", ya: " << ya.transpose()  << ", za: " << za.transpose() << ", fq: " << fq << std::endl;
+      // Rcpp::Rcout << "xa: " << xa.transpose() << ", ya: " << ya.transpose()  << ", za: " << za.transpose() << ", fq: " << fq << std::endl;
   
-    result(u) = fq * A.colPivHouseholderQr().solve(za);
-  
+      result(u) = fq * A.colPivHouseholderQr().solve(za);
+    }
   }
   
   return ( result ); 
--- tests/testthat/test_interp2lin.R
+++ tests/testthat/test_interp2lin.R
@@ -4,7 +4,7 @@
 library(testthat)
 
 # These check out OK.
-U = test_that("basic pracma::interp2 example gives same output ", { 
+U1 = test_that("basic pracma::interp2 example gives same output ", { 
 
   x <- linspace(-1, 1, 11)
   y <- linspace(-1, 1, 11)
@@ -18,10 +18,8 @@ U = test_that("basic pracma::interp2 example gives same output ", {
 
 })
 
-
-
 # These check out OK.
-U = test_that("basic pracma::interp2 (extended) example gives same output ", {
+U2 = test_that("basic pracma::interp2 (extended) example gives same output ", {
 
   x <- linspace(-1, 4, 11)
   y <- linspace(-1, 4, 11)
@@ -32,32 +30,53 @@ U = test_that("basic pracma::interp2 (extended) example gives same output ", {
   AA = interp2(x, y, Z, xp, yp, method)
   BB = interp2lin( x, y, Z, xp, yp)
   expect_equal(sum(AA), sum(BB), tolerance = 2e-15)
-
 })
 
+# These check out OK.
+U3 = test_that("basic pracma::interp2 (extended) example gives same output and out of sample", {
 
-x <- linspace(-1, 4, 101)
-y <- linspace(-1, 4, 101)
-mgrid <- meshgrid(x, y)
-Z <- mgrid$X^2 + 2*mgrid$Y + 3
-xout <- linspace(-1, 4, 200)
-meshOut <- meshgrid(xout)
-xp <- meshOut$X
-yp <- meshOut$Y
-method <- "linear"
-system.time({AA = interp2(x, y, Z, xp, yp, method)})
-system.time({BB = interp2lin( x, y, Z, xp, yp)})
-expect_equal(sum(AA), sum(BB), tolerance = 2e-15)
+  x <- linspace(-1, 4, 11)
+  y <- linspace(-1, 4, 11)
+  mgrid <- meshgrid(x, y)
+  Z <- mgrid$X^2 + 2*mgrid$Y + 3
+  xp <- yp <- linspace(-1, 5, 51)
+  method <- "linear"
+  AA = interp2(x, y, Z, xp, yp, method)
+  BB = interp2lin( x, y, Z, xp, yp)
+  expect_equal(sum(AA, na.rm=TRUE), sum(BB, na.rm=TRUE), tolerance = 2e-15)
+  expect_equal(is.na(BB), is.na(AA), tolerance = 2e-15)
+})
 
-x <- linspace(-1, 4, 1001)
-y <- linspace(-1, 4, 1001)
-mgrid <- meshgrid(x, y)
-Z <- mgrid$X^2 + 2*mgrid$Y + 3
-xout <- linspace(-1, 4, 100)
-meshOut <- meshgrid(xout)
-xp <- meshOut$X
-yp <- meshOut$Y
-method <- "linear"
-system.time({AA = interp2(x, y, Z, xp, yp, method)})
-system.time({BB = interp2lin( x, y, Z, xp, yp)})
-expect_equal(sum(AA), sum(BB), tolerance = 2e-15)
+#These check out OK.
+U4 = test_that("basic pracma::interp2 example gives same outputs large output grid", {
+
+  x <- linspace(-1, 4, 101)
+  y <- linspace(-1, 4, 101)
+  mgrid <- meshgrid(x, y)
+  Z <- mgrid$X^2 + 2*mgrid$Y + 3
+  xout <- linspace(-1, 4, 200)
+  meshOut <- meshgrid(xout)
+  xp <- meshOut$X
+  yp <- meshOut$Y
+  method <- "linear"
+  system.time({AA = interp2(x, y, Z, xp, yp, method)})
+  system.time({BB = interp2lin( x, y, Z, xp, yp)})
+  expect_equal(sum(AA), sum(BB), tolerance = 2e-15)
+})
+
+#These check out OK.
+U5 = test_that("basic pracma::interp2 example gives same outputs large input grid", {
+
+  x <- linspace(-1, 4, 1001)
+  y <- linspace(-1, 4, 1001)
+  mgrid <- meshgrid(x, y)
+  Z <- mgrid$X^2 + 2*mgrid$Y + 3
+  xout <- linspace(-1, 4, 100)
+  meshOut <- meshgrid(xout)
+  xp <- meshOut$X
+  yp <- meshOut$Y
+  method <- "linear"
+  system.time({AA = interp2(x, y, Z, xp, yp, method)})
+  system.time({BB = interp2lin( x, y, Z, xp, yp)})
+  expect_equal(sum(AA), sum(BB), tolerance = 2e-15)
+})
