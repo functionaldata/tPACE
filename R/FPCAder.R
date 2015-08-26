2132bd706b3678a1bc2247086b11bb54004376f1
Pantelis Hadjipantelis <pantelis@bernoulli.ucdavis.edu>
2015-08-25 23:16:41 -0700

    Added full PACE-QUO functionality.

--- R/FPCAder.R
+++ R/FPCAder.R
@@ -3,6 +3,7 @@
 #' Derivative FPCA for dense or sparse functional data. 
 #' 
 #' @param fpcaObj A object of class FPCA returned by the function FPCA(). 
+#' @param variant A string specifying the methodology used ('simple', 'QUO'; default: 'simple')
 #'
 #' @examples
 #' set.seed(1)
@@ -17,20 +18,36 @@
 #' @export
 
 
-FPCAder <-  function (fpcaObj) {
+FPCAder <-  function (fpcaObj, variant = 'simple') {
   # Use FPCA object to get derivative information object 
    
   if (class(fpcaObj) != 'FPCA'){
     stop("FPCAder() requires an FPCA class object as basic input")
   }
-  
-  fpcaObjDer <- list( 
-    phi = apply(fpcaObj$phi, 2, getDerivative, t= fpcaObj$workGrid),
-    mu = getDerivative(y = fpcaObj$mu, t = fpcaObj$obsGrid), 
-    obsGrid = fpcaObj$obsGrid)
-     
-  class(fpcaObjDer) <- 'FPCAder'
-  return(fpcaObjDer) 
+  if( variant == 'simple') {
+    fpcaObjDer <- list( 
+      phi = apply(fpcaObj$phi, 2, getDerivative, t= fpcaObj$workGrid),
+      mu = getDerivative(y = fpcaObj$mu, t = fpcaObj$obsGrid), 
+      obsGrid = fpcaObj$obsGrid)
+       
+    class(fpcaObjDer) <- 'FPCAder'
+    return(fpcaObjDer) 
+  } else if ( variant == 'QUO' ){
+    impSample <- fitted(fpcaObj);
+    impSampleDer <- t(apply( impSample,1,getDerivative, fpcaObj$workGrid));
+    N = dim(impSample)[1];
+    M = dim(impSample)[2];
+    L = makePACEinputs(IDs = rep(1:N,each=M), tVec=rep(fpcaObj$workGrid,N), as.vector(t(impSampleDer)))
+    prefpcaObjDer = FPCA(y= L$Ly, t= L$Lt)
+    fpcaObjDer = list( 
+      phi = prefpcaObjDer$phi, mu =  prefpcaObjDer$mu, obsGrid = fpcaObj$obsGrid)     
+
+    class(fpcaObjDer) <- 'FPCAder'
+   return(fpcaObjDer)
+  } else {
+    stop("Invalid FPCAder variant requested.")
+    return( NULL )
+  }
 } 
 
 getEnlargedGrid <- function(x){ 
@@ -38,10 +55,13 @@ getEnlargedGrid <- function(x){
   return (  c( x[1] - 0.1 * diff(x[1:2]), x, x[N] + 0.1 * diff(x[(N-1):N])) )
 }
 
-getDerivative <- function(y,t){ 
+getDerivative <- function(y,t){
+  if( length(y) != length(t) ){
+    stop("getDerivative y/t lengths are unequal.")    
+  }
   newt = getEnlargedGrid(t)
-  newy = approxExtrap(x=t, y=y, xout= newt)$y 
-  return (numDeriv::grad( splinefun(newt, newy) , x = t ) )
+  newy = Hmisc::approxExtrap(x=t, y=y, xout= newt)$y 
+  return (numDeriv::grad( stats::splinefun(newt, newy) , x = t ) )
 }
 
 
