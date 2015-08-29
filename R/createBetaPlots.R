3b3c910b50d78a6f647f78bcea9e35275f6db844
Pantelis <p.z.hadjipantelis@gmail.com>
2015-08-28 19:05:41 -0700

    Plotting utility for functional betas.

---
+++ R/createBetaPlots.R
@@ -0,0 +1,22 @@
+createBetaPlots <- function(fpcaRegObj,sList, openNewDev = TRUE){
+ 
+  if(openNewDev){ 
+    dev.new(width=7.95, height=3.0, noRStudioGD=TRUE) ; 
+  }
+
+  noOfBetas <- length(fpcaRegObj$betaFunc)  
+  xlabelString = 's';  
+  par(mfrow = c(1,noOfBetas))
+  yedge = max(abs(unlist(fpcaRegObj$betaFunc))) *1.1
+  
+  for (i in 1:noOfBetas){
+    ylabelString = paste('beta function ',  i ,   sep=''   )
+    plot(sList[[i]], fpcaRegObj$betaFunc[[i]], type= 'l', 
+      xlab= xlabelString, ylab=ylabelString    )
+    abline(v=(seq(min(sList[[i]]), max(sList[[i]]), length.out=  diff( range(sList[[i]])) +1 )), 
+      col="lightgray", lty="dotted")
+  }
+}
+
+
+
