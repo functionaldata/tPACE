0c950a6a7b06e53773dda0261afee3b0cc41c096
Pantelis <p.z.hadjipantelis@gmail.com>
2015-09-05 17:07:33 -0700

    FPCAregFunc ironing out.

--- R/FPCAregFunc.R
+++ R/FPCAregFunc.R
@@ -32,14 +32,14 @@ FPCAregFunc <- function(depVar,  expVarScal = NULL, expVarFunc = NULL, regressio
   if( regressionType == 'Dense' ){
     fittedCurves = fitted(depVar)
     if (!is.null(expVarFunc)){
-      fittedPredCurvesList <- lapply( fpcaObjList, fitted)
+      fittedPredCurvesList <- lapply( expVarFunc, fitted)
     } else {
       fittedPredCurvesList <- NULL
     } 
     for (i in 1:length(depVar$workGrid)) {
       fitValuesAtTimeT = fittedCurves[,i];
       for (j in 1:length(expVarFunc)){
-        eval(parse(text=paste("Zvariables$funcPredictor", j, " = fittedPredCurvesList[[i]][,j]", sep='')))
+        eval(parse(text=paste("Zvariables$funcPredictor", j, " = fittedPredCurvesList[[j]][,i]", sep='')))
       }
       BetaFunctions[,i] =  coef( lm(fitValuesAtTimeT ~ ., data = Zvariables))[2: (1 + P)]
     }
@@ -58,26 +58,26 @@ FPCAregFunc <- function(depVar,  expVarScal = NULL, expVarFunc = NULL, regressio
     } else {
 
       Q =  length(expVarFunc)
-      L =  length(fpcaObjList[[1]]$workGrid)
+      L =  length(expVarFunc[[1]]$workGrid)
 
       # Construct the matrix CCXX auto- and cross- covariances for the functional predictors
       CCXX = matrix( rep(0, ( L* Q)^2 ), nrow = L* Q ) 
       for(i in 1:( Q -0)){
-        if( !all.equal(fpcaObjList[[i]]$workGrid, depVar$workGrid) ){
+        if( !all.equal(expVarFunc[[i]]$workGrid, depVar$workGrid) ){
           stop('You need the same workGrid for the functional predictors') 
         }
       }
       for( i in 1:Q){
         for( j in i:Q){
           if(i == j){
-            CCXX[ ((i-1)*L) + (1:L), ((j-1)*L) + (1:L) ] = fpcaObjList[[j]]$fittedCov
+            CCXX[ ((i-1)*L) + (1:L), ((j-1)*L) + (1:L) ] = expVarFunc[[j]]$fittedCov
           } else {
-            tempCCXX =  CrCovYX(Ly1 = fpcaObjList[[i]]$inputData$y, Ly2 = fpcaObjList[[j]]$inputData$y,
-                               Lt1 = fpcaObjList[[i]]$inputData$t, Lt2 = fpcaObjList[[j]]$inputData$t,
-                               Ymu1= fpcaObjList[[i]]$mu,  Ymu2= fpcaObjList[[j]]$mu);
-            CCXX[ ((i-1)*L) + (1:L), ((j-1)*L) + (1:L) ] = tempCCX$smoothedCC;
+            tempCCXX =  CrCovYX(Ly1 = expVarFunc[[i]]$inputData$y, Ly2 = expVarFunc[[j]]$inputData$y,
+                               Lt1 = expVarFunc[[i]]$inputData$t, Lt2 = expVarFunc[[j]]$inputData$t,
+                               Ymu1= expVarFunc[[i]]$mu,  Ymu2= expVarFunc[[j]]$mu, bw1 = bwFunct[1], bw2 = bwFunct[2]);
+            CCXX[ ((i-1)*L) + (1:L), ((j-1)*L) + (1:L) ] = tempCCXX$smoothedCC;
             # And the transpose
-            CCXX[ ((j-1)*L) + (1:L), ((i-1)*L) + (1:L) ] = t(tempCCX$smoothedCC);
+            CCXX[ ((j-1)*L) + (1:L), ((i-1)*L) + (1:L) ] = t(tempCCXX$smoothedCC);
           }
         }  
       }
@@ -85,9 +85,9 @@ FPCAregFunc <- function(depVar,  expVarScal = NULL, expVarFunc = NULL, regressio
       # Construct the matrix CCYX for the cross-covariance between the func. predictors and the func. response variable
       CCYX =  matrix( rep(0, (L* L* Q) ), ncol = L )
       for (i in 1:Q){
-        tempCCYX = CrCovYX(Ly1 = fpcaObjList[[i]]$inputData$y, Ly2 = depVar$inputData$y,
-                                Lt1 = fpcaObjList[[i]]$inputData$t, Lt2 = depVar$inputData$t,
-                                Ymu1= fpcaObjList[[i]]$mu,  Ymu2= depVar$mu);
+        tempCCYX = CrCovYX(Ly1 = expVarFunc[[i]]$inputData$y, Ly2 = depVar$inputData$y,
+                                Lt1 = expVarFunc[[i]]$inputData$t, Lt2 = depVar$inputData$t,
+                                Ymu1= expVarFunc[[i]]$mu,  Ymu2= depVar$mu,  bw1 = bwFunct[1], bw2 = bwFunct[2]);
         CCYX[((i-1)*L) + (1:L),] = t(tempCCYX$smoothedCC);
       }
 
@@ -96,28 +96,30 @@ FPCAregFunc <- function(depVar,  expVarScal = NULL, expVarFunc = NULL, regressio
       for (i in 1:Q){
         for (j in 1:ncol(expVarScal)){
           CCXZ[j+ (i-1)* ncol(expVarScal), ] = 
-                     CrCovYZ (Ymu = fpcaObjList[[i]]$mu, Lt = fpcaObjList[[i]]$inputData$t, Ly = fpcaObjList[[i]]$inputData$y, 
-                              Z = Zvariables[,j],support = fpcaObjList[[i]]$workGrid, bw = bwScalar)$smoothedCC
+                     CrCovYZ (Ymu = expVarFunc[[i]]$mu, Lt = expVarFunc[[i]]$inputData$t, Ly = expVarFunc[[i]]$inputData$y, 
+                              Z = Zvariables[,j],support = expVarFunc[[i]]$workGrid, bw = bwScalar)$smoothedCC
         }
       }
 
       for( i in 1:length(depVar$workGrid)) {
         covYZX = matrix(rep(0,P),1)
         covZX = matrix(rep(0,P^2),P)
-
+# browser()
         covYZX[1:(P-Q)] = CCYZ[i,];
         covYZX[(P-Q+1):P] =CCYX[ i+ (0:(Q-1))*L, i] ; 
+       
         covZX[1:(P-Q),  1:(P-Q)] = cov(Zvariables);
-        covZX[1:(P-Q),  (P-Q+1):P] =   matrix( CCXZ[1:(P-Q), i])
-        covZX[(P-Q+1):P,  1:(P-Q)] = t(matrix( CCXZ[1:(P-Q), i]))
+        covZX[1:(P-Q),  (P-Q+1):P] = t( matrix( CCXZ[, i], 2, byrow=TRUE))
+        covZX[(P-Q+1):P,  1:(P-Q)] =    matrix( CCXZ[, i], 2, byrow=TRUE)
         for( j0 in 1:Q){
           for( j1 in 1:Q){
-            covZX[ P-Q+j0, P-Q+j1] = CCXX[ j0*L , j1*L]
+            covZX[ P-Q+j0, P-Q+j1] = CCXX[i + (j0-1)*L , i + (j1-1)*L]
           }
         }     
         BetaFunctions[,i] = solve( covZX, t(covYZX))
       }
     }
   }
-  return(BetaFunctions)
+  FRegObj <- list(betaFunctions = BetaFunctions)
+  return(FRegObj)
 }
