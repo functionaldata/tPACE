04047d1fe303ccf356189bdfab2353267954682f
hadjipantelis <p.z.hadjipantelis@gmail.com>
2015-11-14 14:28:20 -0800

    Check for NA/NaNs in input values.

--- R/Rlwls1d.R
+++ R/Rlwls1d.R
@@ -1,6 +1,6 @@
 #' One dimensional local linear kernel smoother
 #' 
-#' One dimensional local linear kernel smoother for sparse data.
+#' One dimensional local linear kernel smoother for longitudinal data.
 #'
 #' @param bw Scalar holding the bandwidth
 #' @param kernel_type Character holding the kernel type (see ?FPCA for supported kernels)
@@ -17,6 +17,15 @@
 
 
 Rlwls1d <- function( bw, kernel_type, win, xin, yin, xout, npoly = 1L, nder = 0L){
+
+  # Deal with NA/NaN measurement values
+  NAinY = is.na(yin);
+  if any(NANinY){
+      win = win[!NAinY]
+      xin = xin[!NAinY]
+      yin = yin[!NAinY]
+  }
+
   return( CPPlwls1d(bw= as.numeric(bw), kernel_type = kernel_type, npoly= as.integer(npoly), nder= as.integer(nder), 
                   xin = as.numeric(xin), yin= as.numeric(yin), xout= as.numeric(xout), win = as.numeric(win)))
 }
