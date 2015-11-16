01fd03659989bcf86297b04540446debcc3d2ff1
Xiongtao Dai <dai@ucdavis.edu>
2015-11-15 22:25:04 -0800

    wiener speed test

--- tests/testthat/test_RmatLwls2d.R
+++ tests/testthat/test_RmatLwls2d.R
@@ -10,8 +10,8 @@ matXinYinWin <- function(xin, yin, win) {
   wMat <- yMat
 
   for (i in seq_len(N)) {
-    yMat[x1[i], x2[i]] <- yMat[x1[i], x2[i]] + yin[i]
-    wMat[x1[i], x2[i]] <- wMat[x1[i], x2[i]] + 1
+    yMat[x1[i], x2[i]] <- yMat[x1[i], x2[i]] + yin[i] * win[i]
+    wMat[x1[i], x2[i]] <- wMat[x1[i], x2[i]] + win[i]
   }
   yMat <- yMat / wMat
 
@@ -48,8 +48,8 @@ test_that('Mat 2D smoother is the same as the long 2D smoother, unique time poin
 ## time points are on a finite grid
 set.seed(1)
 n <- 1e4
-bw <- 0.2
-xin <- round(matrix(runif(2 * n), n, 2), 2)
+bw <- 0.1
+xin <- round(matrix(runif(2 * n), n, 2), 3)
 yin <- rnorm(n)
 win <- rep(1, n)
 xout1 <- xout2 <- seq(0, 1, length.out=5)
@@ -61,6 +61,23 @@ test_that('Mat 2D smoother is the same as the long 2D smoother', {
   expect_equal(tmp, t(tmp1))
 })
 
-# speed test
-microbenchmark(with(matList, RmatLwls2d(c(bw, bw), 'epan', yMat, wMat, x1Grid, x2Grid, xout1, xout2, FALSE, FALSE)), times=100L)
-microbenchmark(RmullwlskCC(c(bw, bw), 'epan', t(xin), yin, win, xout1, xout2, FALSE), times=100L)
+# microbenchmark(with(matList, RmatLwls2d(c(bw, bw), 'epan', yMat, wMat, x1Grid, x2Grid, xout1, xout2, FALSE, FALSE)), times=100L)
+# microbenchmark(RmullwlskCC(c(bw, bw), 'epan', t(xin), yin, win, xout1, xout2, FALSE), times=100L)
+
+
+## Functional setting
+set.seed(1)
+bw <- 0.1
+pts <- seq(0, 1, length.out=100L)
+mu <- rep(0, length(pts))
+a <- wiener(200, pts, sparsify=5:10)
+xout1 <- xout2 <- seq(0, 1, length.out=5)
+rcov <- BinRawCov(GetRawCov(a$yList, a$tList, pts, mu, 'Sparse', 0))
+matList <- matXinYinWin(rcov$tPairs, rcov$meanVals, rcov$count)
+tmp <- with(matList, RmatLwls2d(c(bw, bw), 'epan', yMat, wMat, x1Grid, x2Grid, xout1, xout2, FALSE, FALSE))
+tmp1 <- with(rcov, RmullwlskCC(c(bw, bw), 'epan', t(tPairs), meanVals, count, xout1, xout2, FALSE))
+expect_equal(tmp, t(tmp1))
+
+# # speed test
+# microbenchmark(tmp <- with(matList, RmatLwls2d(c(bw, bw), 'epan', yMat, wMat, x1Grid, x2Grid, xout1, xout2, FALSE, FALSE)), times=100L)
+# microbenchmark(tmp1 <- with(rcov, RmullwlskCC(c(bw, bw), 'epan', t(tPairs), meanVals, count, xout1, xout2, FALSE)), times=100L)
