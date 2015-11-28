73f1e646ffc7b8f092977d10bfba612ab01950bb
Xiongtao Dai <dai@ucdavis.edu>
2015-11-28 02:31:24 -0800

    added test_useBin.R and generated documentations

--- tests/testthat/test_useBin.R
+++ tests/testthat/test_useBin.R
@@ -1,6 +1,15 @@
-## The binned version is exactly the same as the unbinned version.
-set.seed(1)
-pts <- seq(0, 1, by=0.01)
-samp3 <- wiener(2000, pts, sparsify=1:length(pts))
-y <- unlist(samp3$yList)
-x <- unlist(samp3$tList)
+devtools::load_all()
+library(testthat)
+
+test_that('The binned version is exactly the same as the unbinned version.', {
+  set.seed(1)
+  pts <- seq(0, 1, by=0.05)
+  samp3 <- wiener(300, pts, sparsify=5)
+  y <- samp3$yList
+  t <- samp3$tList
+
+  resNoBin <- FPCA(y, t, list(datatype='Sparse', useBinnedData='OFF', useBinnedCov=FALSE))
+  resBin <- FPCA(y, t, list(datatype='Sparse', useBinnedData='OFF', useBinnedCov=TRUE))
+
+  expect_equal(resNoBin[names(resNoBin) != 'optns'], resBin[names(resBin) != 'optns'])
+})
