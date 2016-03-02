129fc7ff4f04e35251ae7a22dd6fd13d50ff54d1
hadjipantelis <p.z.hadjipantelis@gmail.com>
2016-03-01 16:39:39 -0800

    WHO z-score functions and dataset renaming.

--- R/medfly25.R
+++ R/medfly25.R
@@ -7,12 +7,11 @@
 #'
 #' @name medfly25
 #' @docType data
-#' @format A data frame with 789 rows and 26 variables:
+#' @format A data frame with 19725 rows and 3 variables:
 #' \describe{
 #' \item{ID}{: Medfly ID according to the orignal dataset}
-#' \item{Day1}{: Number of eggs laid at day 1}
-#' \item{Day2}{: Number of eggs laid at day 2}
-#' \item{...}{...}
+#' \item{Days}{: Day of measurement}
+#' \item{nEggs}{: Number of eggs laid at that particular day} 
 #' } 
 #' @source \url{http://anson.ucdavis.edu/~mueller/data/medfly1000.html}
 #' @references{Carey, J.R., Liedo, P., Mueller, H.G., Wang, J.L., Chiou, J.M. (1998). Relationship of age patterns of fecundity to mortality, longevity, and lifetime reproduction in a large cohort of Mediterranean fruit fly females. J. of Gerontology --Biological Sciences 53, 245-251. }
