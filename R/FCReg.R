05ee40391857fddc4e830a07199286059e204c98
Xiongtao Dai <dai@ucdavis.edu>
2016-05-15 22:12:37 -0700

    add FCReg example

--- R/FCReg.R
+++ R/FCReg.R
@@ -20,6 +20,42 @@
 #'
 #' \cite{Senturk, D., Nguyen, D.V. "Varying Coefficient Models for Sparse Noise-contaminated Longitudinal Data", Statistica Sinica 21(4), (2011): 1831-1856. (Sparse data)} 
 #' @export
+#' @examples 
+#' # Y(t) = \beta_0(t) + \beta_1(t) X_1(t) + \beta_2(t) Z_2 + \epsilon
+#' 
+#' # Settings
+#' set.seed(1)
+#' n <- 100
+#' nGridIn <- 200
+#' sparsity <- 5:10 # Sparse data sparsity
+#' T <- round(seq(0, 1, length.out=nGridIn), 4) # Functional data support
+#' bw <- 0.1
+#' outGrid <- round(seq(min(T), 1, by=0.05), 2)
+#' 
+#' # Simulate functional data 
+#' mu <- T * 2 # mean function for X_1
+#' sigma <- 1
+#' 
+#' beta_0 <- 0
+#' beta_1 <- 1
+#' beta_2 <- 1
+#' 
+#' Z <- MASS::mvrnorm(n, rep(0, 2), diag(2))
+#' X_1 <- Z[, 1, drop=FALSE] %*% matrix(1, 1, nGridIn) + matrix(mu, n, nGridIn, byrow=TRUE)
+#' epsilon <- rnorm(n, sd=sigma)
+#' Y <- matrix(NA, n, nGridIn)
+#' for (i in seq_len(n)) {
+#'   Y[i, ] <- beta_0 + beta_1 * X_1[i, ] + beta_2 * Z[i, 2] + epsilon[i]
+#' }
+#' 
+#' # Sparsify functional data
+#' set.seed(1)
+#' X_1sp <- Sparsify(X_1, T, sparsity)
+#' set.seed(1)
+#' Ysp <- Sparsify(Y, T, sparsity)
+#' vars <- list(X_1=X_1sp, Z_2=Z[, 2], Y=Ysp)
+#' withError2D <- FCReg(vars, bw, bw, outGrid)
+
 
 FCReg <- function(vars, userBwMu, userBwCov, outGrid, kern='gauss', measurementError=TRUE, diag1D='none', useGAM = FALSE, returnCov=TRUE) {
   
