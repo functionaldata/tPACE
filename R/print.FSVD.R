05525972664a5cb89435c0769ce63cfd95b8d94a
hadjipantelis <p.z.hadjipantelis@gmail.com>
2016-06-14 02:45:53 -0700

    Deleted print.FSVD example.

--- R/print.FSVD.R
+++ R/print.FSVD.R
@@ -5,15 +5,6 @@
 #' @param x An FSVD object.
 #' @param ... Not used.
 #'
-#' @examples
-#' set.seed(1)
-#' n <- 20
-#' pts <- seq(0, 1, by=0.05)
-#' sampWiener <- Wiener(n, pts)
-#' sampWiener <- Sparsify(sampWiener, pts, 10)
-#' res <- FSVD(sampWiener$Ly, sampWiener$Lt)
-#' res
-#'
 #' @method print FSVD
 #' @export
 print.FSVD <- function(x, ...){
