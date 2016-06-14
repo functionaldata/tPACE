78b08d6dc1f35e6a6e51cfe3f31864a41426b43e
hadjipantelis <p.z.hadjipantelis@gmail.com>
2016-06-14 13:47:10 -0700

    Trying to run all the unitests on Travis..

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,6 +1,5 @@
 # # Based on https://github.com/hadley/testthat#integration-with-r-cmd-check
 
-# library(testthat)
-# library(tPACE)
-
-# test_check("tPACE")
+library(testthat)
+library(fdapace)
+test_check("fdapace")
