b77522355020ad844170c24b7ba452f22c0f69f4
Xiongtao Dai <dai@ucdavis.edu>
2016-08-21 13:37:00 -0700

    fix basis test

--- tests/testthat/test_CreateBasis.R
+++ tests/testthat/test_CreateBasis.R
@@ -5,10 +5,10 @@ test_that('legendre basis works', {
   pts <- seq(0, 1, length.out=M)
   K <- 10
 
-  tmp <- CreateBasis(K, pts, 'poly')
+  tmp <- CreateBasis(K, pts, 'legendre01')
   expect_equal(crossprod(tmp) / M, diag(K), scale=1, tol=1e-3)
 
   K <- 1
-  tmp <- CreateBasis(K, pts, 'poly')
+  tmp <- CreateBasis(K, pts, 'legendre01')
   expect_equal(crossprod(tmp) / M, matrix(1), scale=1, tol=1e-3)
 })
