435a388193cbc908040bb71478ff12f95e6509b7
hadjipantelis <p.z.hadjipantelis@gmail.com>
2016-12-07 23:35:25 +0000

    Correcting test.

--- tests/testthat/test_RcppPseudoApprox.R
+++ tests/testthat/test_RcppPseudoApprox.R
@@ -10,8 +10,7 @@ test_that('RcppPseudoApprox works on a nearly trivial example', {
 
 test_that('RcppPseudoApprox errs on obviously wrong data.', { 
   
-  expect_equal( RcppPseudoApprox(X = c(0,1), Y = c(0,2,4) , X_target = 0), 2*z, tolerance = 1e-7)
-  expect_error( RcppPseudoApprox(X = c(0,1), Y = c(0,2,4) , X_target = 0), 
+   expect_error( RcppPseudoApprox(X = c(0,1), Y = c(0,2,4) , X_target = 0), 
                  "Problem with unequal vector sizes when doing linear interpolation.")
   
 }) 
