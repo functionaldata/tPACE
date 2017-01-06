f99dbe4c885ceb2359319c40fbfc8a5cc79f798d
hadjipantelis <p.z.hadjipantelis@gmail.com>
2017-01-06 18:34:49 +0200

    WFDA Unit Test & Example

---
+++ tests/testthat/test_WFDA.R
@@ -0,0 +1,60 @@
+# devtools::load_all()
+library(testthat)
+
+test_that('noisy dense data, default arguments', {
+  
+  N = 44;
+  eps = 0.23;
+  M = 41;
+  set.seed(123) 
+  Tfinal = 3
+  me <- function(t) exp(-Tfinal*(((t/Tfinal^2)-0.5))^2);
+  T = seq(0,Tfinal,length.out = M) 
+  recondingTimesMat = matrix(nrow = N, ncol = M)
+  yMat = matrix(nrow = N, ncol = M)
+  
+  for (i in 1:N){
+    peak = runif(min = 0.25,max =  0.75,1)*Tfinal 
+    recondingTimesMat[i,] = Tfinal* sort( unique(c( seq(0.0 , peak, length.out = round((M+1)/2)),
+                                                    seq( peak, Tfinal, length.out = round((M+1)/2))) ))
+    yMat[i,] = me(recondingTimesMat[i,])* rnorm(1, mean=4.0, sd=  eps)    + rnorm(M, mean=0.0, sd=  eps) 
+  }
+  
+  Y = as.list(as.data.frame(t(yMat)))
+  X = rep(list(T),N)
+  
+  sss =  WFDA(Ly = Y, Lt = X )
+  
+  expect_gt( abs(  cor( colMeans(sss$aligned), me(T*3) ) ), 0.996) 
+  expect_gt( abs(  min(apply( sss$aligned, 1, function(u) cor( u, me(T*3) ) )) ), 0.925)
+  
+})
+
+test_that('noiseless dense data - only phase variations, default arguments', {
+  
+  N = 44;
+  eps = 0.000;
+  M = 41;
+  set.seed(123) 
+  Tfinal = 3
+  me <- function(t) exp(-Tfinal*(((t/Tfinal^2)-0.5))^2);
+  T = seq(0,Tfinal,length.out = M) 
+  recondingTimesMat = matrix(nrow = N, ncol = M)
+  yMat = matrix(nrow = N, ncol = M)
+  
+  for (i in 1:N){
+    peak = runif(min = 0.25,max =  0.75,1)*Tfinal 
+    recondingTimesMat[i,] = Tfinal* sort( unique(c( seq(0.0 , peak, length.out = round((M+1)/2)),
+                                                    seq( peak, Tfinal, length.out = round((M+1)/2))) ))
+    yMat[i,] = me(recondingTimesMat[i,])* rnorm(1, mean=4.0, sd=  eps)    + rnorm(M, mean=0.0, sd=  eps) 
+  }
+  
+  Y = as.list(as.data.frame(t(yMat)))
+  X = rep(list(T),N)
+  
+  sss =  WFDA(Ly = Y, Lt = X )
+  
+  expect_gt( abs(  cor( colMeans(sss$aligned), me(T*3) ) ), 0.996) 
+  expect_gt( abs(  min(apply( sss$aligned, 1, function(u) cor( u, me(T*3) ) )) ), 0.965)
+  
+})
