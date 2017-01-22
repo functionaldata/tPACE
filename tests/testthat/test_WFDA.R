58b4bfde61f7d4f5ed086b8f1b97eb5da8b09eef
hadjipantelis <p.z.hadjipantelis@gmail.com>
2017-01-22 19:36:45 +0000

    MATLAB compatibiity WFDA test

--- tests/testthat/test_WFDA.R
+++ tests/testthat/test_WFDA.R
@@ -58,3 +58,27 @@ test_that('noiseless dense data - only phase variations, default arguments', {
   expect_gt( abs(  min(apply( sss$aligned, 1, function(u) cor( u, me(T*3) ) )) ), 0.965)
   
 })
+
+test_that('R implemenation are at least as good as current MATLAB WFPCA implementation in computing the mean',{
+  
+  load(system.file('testdata', 'YmatFromWFPCAexample_rng123.RData', package='fdapace'))
+ 
+  Y = as.list(as.data.frame(t(QQ)))
+  N = nrow(QQ)
+  T = seq(0,1,length.out = ncol(QQ))
+  X = rep(list(T),N)
+  sss =  WFDA(Ly = Y, Lt = X )
+  
+  # In MATLAB
+  # >> sum((mean(aligned) - me(T)).^2)
+  # ans =
+  #   0.0213
+  # >> sum(abs(mean(aligned) - me(T)).^1)
+  # ans =
+  #   0.4686
+  
+  meTRUE = exp(-10*(T-0.5)^2); # True mean
+  expect_lt(sum(abs(meTRUE- colMeans(sss$aligned))), 0.4686) 
+  expect_lt(sum((meTRUE- colMeans(sss$aligned))^2), 0.0213) 
+  
+})
