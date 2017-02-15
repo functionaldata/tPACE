f5a03ebcc8151fa2ccf79ad428f74aca50bab6ad
dmchris <haoji@ucdavis.edu>
2017-02-14 22:20:05 -0800

    tentative codes for stringing
    Feature various kinds of dissimilarity metrics. Uses UDS via function
    MASS::isoMDS. Performance is better than Matlab. Standardization of
    input data not yet implemented (could add an option, but Matlab version
    doesn’t have this part.)

---
+++ R/Stringing.R
@@ -0,0 +1,113 @@
+#' Stringing for High-Dimensional data
+#' 
+#' @param X A matrix (n by p) of data, where X[i,] is the row vector of measurements for the ith subject.
+#' @param Y A vector (n by 1), where Y[i] is the reponse associated with X[i,]
+#' @param disOptns A distance metric to be used, one of the following: "euclidean" (default), "correlation", "spearman", "hamming", "xycor" or "user". If specified as "xycor", the absolute difference of correlation between predictor and response is used. If specified as "user", a dissimilarity matrix for the argument "disMat" must be provided.
+#' @param disMat A user-specified dissimilarity matrix, only necessary when "disOptns" is "user".
+#' @param isPlot A logical variable indicating whether to plot the stringing function, default: TRUE.
+#' 
+#' @return A list containing the following fields:
+#' \item{Ly}{A list of n vectors, which are the random trajectories for all subjects identified by the Stringing method.}
+#' \item{Lt}{A list of n time points vectors, at which corresponding measurements Ly are taken.}
+#' \item{stringedPos}{A vector, indicating positions of corresponding predictors after stringing.}
+#' @examples
+#' set.seed(1)
+#' n <- 50
+#' wiener = Wiener(n = n)[,-1]
+#' p = ncol(wiener)
+#' rdmorder = sample(size = p, x=1:p, replace = FALSE)
+#' stringingfit = Stringing(X = wiener[,rdmorder], disOptns = "correlation")
+#' diff_norev = sum(abs(rdmorder[stringingfit$stringedPos] - 1:p))
+#' diff_rev = sum(abs(rdmorder[stringingfit$stringedPos] - p:1))
+#' if(diff_rev <= diff_norev){
+#'   stringingfit$stringedPos = rev(stringingfit$stringedPos)
+#'   stringingfit$Ly = lapply(stringingfit$Ly, rev)
+#' }
+#' plot(1:p, rdmorder[stringingfit$stringedPos], pch=18); abline(a=0,b=1)
+#' 
+#' @references
+#' \cite{Chen, K., Chen, K., Mueller, H. G., and Wang, J. L. (2011). "Stringing high-dimensional data for functional analysis." Journal of the American Statistical Association, 106(493), 275-284.}
+#' @export
+
+Stringing = function(X, Y = NULL, disOptns = "euclidean", disMat = NA, isPlot = FALSE){
+  # input check
+  if(!(is.numeric(X) && is.matrix(X))){
+    stop('Incorrect format for input data matrix X! Check if it is a matrix.')
+  }
+  if(!(disOptns %in% c("euclidean", "correlation", "spearman", "hamming", "xycor", "user"))){
+    stop('Invalid distance option specified! Need to be one of "euclidean", "correlation", "spearman", "hamming" and "user".')
+  }
+  if(disOptns == "user" && is.na(disMat)){
+    stop('User specified dissimilarity matrix is missing, which is required for distance option "User".')
+  }
+  if(disOptns == "xycor"){
+    if(is.null(Y)){
+      stop('Missing response vector Y, required when disMat is "xycor".')
+    }
+    if(!(is.numeric(Y) && is.vector(Y))){
+      stop('Incorrect format for input response vector Y! Check if it is a vector.')
+    }
+    if(length(Y) != nrow(X)){
+      stop('Incosistent sample size based on input design matrix X and response vector Y.')
+    }
+  }
+  # Calculate dissimilarity matrix
+  n = nrow(X); p = ncol(X);
+  if(disOptns != "user"){
+    disMat = GetDisMatrix(X, disOptns, Y)
+  }
+  
+  # UDS
+  uds = isoMDS(d = disMat, k = 1, trace = FALSE)
+  pts = uds$points
+  stringedPos = order( (pts - min(pts))/diff(range(pts)) )
+  if(isPlot){
+    plot(1:p, stringedPos, main="Stringing Function",
+         xlab = "Original Order", ylab = "UDS Induced Order")
+  }
+  
+  # obtain stringed data
+  stringedX = X[,stringedPos]
+  stringedTime = 1:p
+  fpcainput = MakeFPCAInputs(IDs = rep(1:n,times=p), tVec = rep(stringedTime, each=n),
+                             yVec = c(stringedX))
+  Ly = fpcainput$Ly
+  Lt = fpcainput$Lt
+  return(list(Ly = Ly, Lt = Lt, stringedPos = stringedPos))
+}
+
+# function to get dissimilarity matrix for given data matrix
+GetDisMatrix = function(X, disOptns = "euclidean", Y){
+  p = ncol(X); n = nrow(X);
+  if(disOptns == "euclidean"){
+    disMat = dist(x = t(X), method = "euclidean")
+  } else if(disOptns == "correlation"){
+    disMat = 1 - cor(X)
+  } else if(disOptns == "spearman"){
+    rankX = X
+    for(i in 1:p){
+      rankX[,i] = rank(X[,i])
+    }
+    disMat = 1 - cor(rankX)
+  } else if(disOptns == "hamming"){
+    disMat = matrix(NA, nrow = p, ncol = p)
+    for(i in 1:p){
+      for(j in i:p){
+        disMat[i,j] = sum(X[,i] != X[,j])/n
+        if(j > i){
+          disMat[j,i] = disMat[i,j]
+        }
+      }
+    }
+  } else { # xy correlation
+    disMat = matrix(NA, nrow = p, ncol = p)
+    XYcor = cor(cbind(Y,X))[-1,1] # the correlation vector of Xj's and Y, j=1,...,p
+    for(i in 1:p){
+      for(j in i:p){
+        disMat[i,j] = abs(XYcor[i] - XYcor[j]);
+        disMat[j,i] = disMat[i,j];
+      }
+    }
+  }
+  return(disMat)
+}
---
+++ tests/testthat/test_Stringing.R
@@ -0,0 +1,135 @@
+# test the performance of Stringing
+library(testthat)
+
+# implement the first kind of simulation in stringing paper
+# Fourier basis, specify signal-to-noise ratio (SNR), number of components used
+# returns true underlying curves XTraj, and randomly ordered design matrix X
+stringing_sim1 <- function(K = 4, SNR = 10, n = 50, p = 50){
+  grid = seq(5/p,5,5/p)
+  lambda = 8*(0.5^(1:K)) # exponential decay
+  phi = cbind(-sqrt(0.2)*cos(0.2*pi*grid), sqrt(0.2)*sin(0.2*pi*grid),
+              -sqrt(0.2)*cos(0.4*pi*grid), sqrt(0.2)*sin(0.4*pi*grid))
+  scores = t(mvrnorm(n = n, mu = rep(0,K), Sigma = diag(lambda)))
+  Xtrue = t(phi %*% scores) # ith row corresponds to ith subject
+  if(SNR == Inf){
+    Xt = Xtrue
+  } else {
+    Xt = Xtrue + rnorm(n*p, mean=0, sd=mean(abs(Xtrue))/SNR)
+  }
+  rdmorder = sample(x = 1:p, size = p, replace = FALSE) # random columns indices for the original order
+  X = Xt[,rdmorder]
+  return(list(XTraj = Xt, X = X, order = rdmorder, n = n, p = p, RegGrid = grid))
+}
+
+test_that("Stringing works perfectly for simulated example without error using correlation metric",{
+  set.seed(1)
+  simdata = stringing_sim1(SNR = Inf)
+  stringingfit = Stringing(simdata$X, disOptns = "correlation", isPlot = FALSE)
+  # check with simulated data to see if reversal of order is needed
+  diff_norev = sum(abs(simdata$order[stringingfit$stringedPos] - 1:simdata$p))
+  diff_rev = sum(abs(simdata$order[stringingfit$stringedPos] - simdata$p:1))
+  if(diff_rev <= diff_norev){
+    stringingfit$stringedPos = rev(stringingfit$stringedPos)
+    stringingfit$Ly = lapply(stringingfit$Ly, rev)
+  }
+  #plot(1:simdata$p, simdata$order[stringingfit$stringedPos], pch=18, 
+  #     xlab="True Order", ylab="Stringed Order")
+  #lines(1:simdata$p, simdata$order[stringingfit$stringedPos])
+  # calculate Relative Order Error (ROE)
+  ERp = (simdata$p-1)*(simdata$p+1)/3 # mean error for randomly sampled orders
+  ROE = sum(abs(simdata$order[stringingfit$stringedPos] - 1:simdata$p))/ERp
+  # the stringing function should recover the random ordering perfectly
+  expect_equal(1:simdata$p, simdata$order[stringingfit$stringedPos])
+  expect_equal(ROE, 0)
+})
+
+test_that("Stringing works for simulated example with small error using correlation metric",{
+  set.seed(1)
+  simdata = stringing_sim1(SNR = 10)
+  stringingfit = Stringing(simdata$X, disOptns = "correlation", isPlot = FALSE)
+  # check with simulated data to see if reversal of order is needed
+  diff_norev = sum(abs(simdata$order[stringingfit$stringedPos] - 1:simdata$p))
+  diff_rev = sum(abs(simdata$order[stringingfit$stringedPos] - simdata$p:1))
+  if(diff_rev <= diff_norev){
+    stringingfit$stringedPos = rev(stringingfit$stringedPos)
+    stringingfit$Ly = lapply(stringingfit$Ly, rev)
+  }
+  # calculate Relative Order Error (ROE)
+  ERp = (simdata$p-1)*(simdata$p+1)/3 # mean error for randomly sampled orders
+  ROE = sum(abs(simdata$order[stringingfit$stringedPos] - 1:simdata$p))/ERp
+  # the stringing function should recover the random ordering perfectly
+  expect_equal(1:simdata$p, simdata$order[stringingfit$stringedPos])
+  expect_equal(ROE, 0)
+})
+
+test_that("Stringing works for simulated example with moderate error using correlation metric",{
+  set.seed(2)
+  simdata = stringing_sim1(SNR = 4)
+  stringingfit = Stringing(simdata$X, disOptns = "correlation", isPlot = FALSE)
+  # check with simulated data to see if reversal of order is needed
+  diff_norev = sum(abs(simdata$order[stringingfit$stringedPos] - 1:simdata$p))
+  diff_rev = sum(abs(simdata$order[stringingfit$stringedPos] - simdata$p:1))
+  if(diff_rev <= diff_norev){
+    stringingfit$stringedPos = rev(stringingfit$stringedPos)
+    stringingfit$Ly = lapply(stringingfit$Ly, rev)
+  }
+  # calculate Relative Order Error (ROE)
+  ERp = (simdata$p-1)*(simdata$p+1)/3 # mean error for randomly sampled orders
+  ROE = sum(abs(simdata$order[stringingfit$stringedPos] - 1:simdata$p))/ERp
+  # the stringing function should recover the random ordering perfectly
+  expect_less_than(ROE, 1e-2)
+})
+
+test_that("Stringing works for simulated example with moderate error using correlation metric",{
+  set.seed(3)
+  simdata = stringing_sim1(SNR = 4)
+  stringingfit = Stringing(simdata$X, disOptns = "correlation", isPlot = FALSE)
+  # check with simulated data to see if reversal of order is needed
+  diff_norev = sum(abs(simdata$order[stringingfit$stringedPos] - 1:simdata$p))
+  diff_rev = sum(abs(simdata$order[stringingfit$stringedPos] - simdata$p:1))
+  if(diff_rev <= diff_norev){
+    stringingfit$stringedPos = rev(stringingfit$stringedPos)
+    stringingfit$Ly = lapply(stringingfit$Ly, rev)
+  }
+  # calculate Relative Order Error (ROE)
+  ERp = (simdata$p-1)*(simdata$p+1)/3 # mean error for randomly sampled orders
+  ROE = sum(abs(simdata$order[stringingfit$stringedPos] - 1:simdata$p))/ERp
+  # the stringing function should recover the random ordering perfectly
+  expect_less_than(ROE, 1e-2)
+})
+
+test_that("Stringing works for simulated example with small error using euclidean distance",{
+  set.seed(4)
+  simdata = stringing_sim1(SNR = 10)
+  stringingfit = Stringing(simdata$X, disOptns = "euclidean", isPlot = FALSE)
+  # check with simulated data to see if reversal of order is needed
+  diff_norev = sum(abs(simdata$order[stringingfit$stringedPos] - 1:simdata$p))
+  diff_rev = sum(abs(simdata$order[stringingfit$stringedPos] - simdata$p:1))
+  if(diff_rev <= diff_norev){
+    stringingfit$stringedPos = rev(stringingfit$stringedPos)
+    stringingfit$Ly = lapply(stringingfit$Ly, rev)
+  }
+  # calculate Relative Order Error (ROE)
+  ERp = (simdata$p-1)*(simdata$p+1)/3 # mean error for randomly sampled orders
+  ROE = sum(abs(simdata$order[stringingfit$stringedPos] - 1:simdata$p))/ERp
+  # the stringing function should recover the random ordering perfectly
+  expect_less_than(ROE, 1e-2)
+})
+
+test_that("Stringing works for simulated example with small error using spearman correlation",{
+  set.seed(5)
+  simdata = stringing_sim1(SNR = 10)
+  stringingfit = Stringing(simdata$X, disOptns = "spearman", isPlot = FALSE)
+  # check with simulated data to see if reversal of order is needed
+  diff_norev = sum(abs(simdata$order[stringingfit$stringedPos] - 1:simdata$p))
+  diff_rev = sum(abs(simdata$order[stringingfit$stringedPos] - simdata$p:1))
+  if(diff_rev <= diff_norev){
+    stringingfit$stringedPos = rev(stringingfit$stringedPos)
+    stringingfit$Ly = lapply(stringingfit$Ly, rev)
+  }
+  # calculate Relative Order Error (ROE)
+  ERp = (simdata$p-1)*(simdata$p+1)/3 # mean error for randomly sampled orders
+  ROE = sum(abs(simdata$order[stringingfit$stringedPos] - 1:simdata$p))/ERp
+  # the stringing function should recover the random ordering perfectly
+  expect_less_than(ROE, 1e-2)
+})
