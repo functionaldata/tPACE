8e899e83256e9d713e5e37d5b7603714738fd441
Kyunghee Han <>
2017-10-24 04:23:10 -0700

    debugging after 1st beta test

--- R/FAM.R
+++ R/FAM.R
@@ -5,31 +5,32 @@
 #' @param Y An \emph{n}-dimensional vector whose elements consist of scalar responses.
 #' @param Lx A list of \emph{n} vectors containing the observed values for each individual. See \code{FPCA} for detail.
 #' @param Lt A list of \emph{n} vectors containing the observation time points for each individual corresponding to y. Each vector should be sorted in ascending order. See \code{FPCA} for detail.
-#' @param K A \code{function} object representing the kernel to be used in the smooth backfitting (default is 'epan', the the Epanechnikov kernel).
-#' @param FVE The fraction of variance explained of of FPCA for each predictor process (default is 0.95).
-#' @param xi An \emph{N} by \emph{K} matrix whose column vectors consist of \emph{N} vectors of estimation points for each component function.
-#' @param h A \emph{K}-dimensional vector of bandwidths for kernel smoothing to estimate each component function (default is NULL, but it automatically apply 10-folds cross-validation for each component function. See Mueller and Yao (2005)),
+#' @param nEval The number of evaluation grid points for kernel smoothing (default is 51. If it is specified as 0, then estimated FPC scores are used for evaluation grid instead of equal grid).
+#' @param bwMethod The method of bandwidth selection for kernel smoothing, a positive value for designating K-fold cross-validtaion and zero for GCV (default is 5)
+#' @param supp The lower and upper limits of kernel smoothing domain for studentized FPC scores (default is [-2,2]).
+#' @param optns A list of options control parameters specified by list(name=value). See \code{FPCA}.
 #'
 #' @details \code{FAM} fits functional additive models for a scalar response and single predictor process proposed by Mueller and Yao (2007) that \deqn{E(Y | \mathbf{X}) = \sum_{k=1}^K g_{k}(\xi_{k}),} where \eqn{\xi_{k}} stand for the k-th FPC score of the the predictor process.
 #'
 #' @return A list containing the following fields:
+#' \item{xi}{An \emph{N} by \emph{K} matrix whose column vectors consist of \emph{N} vectors of estimation points for each component function.}
 #' \item{mu}{Mean estimator of \eqn{EY}}
 #' \item{fam}{A \emph{N} by \emph{K} matrix whose column vectors consist of the component function estimators at the given estimation points.}
-#' \item{xi}{An \emph{N} by \emph{K} matrix whose column vectors consist of \emph{N} vectors of estimation points for each component function.}
+#' \item{bw}{A \emph{K}-dimensional bandwidth vector.}
 #' @examples
 #' 
-#'set.seed(1000)
+#'set.seed(500)
 #'
 #'library(MASS)
-#' 
-#' trapzRcpp <- fdapace:::trapzRcpp
 #'
-#'f1 <- function(t) t
+#'trapzRcpp <- fdapace:::trapzRcpp
+#'
+#'f1 <- function(t) 0.5*t
 #'f2 <- function(t) 2*cos(2*pi*t/4)
-#'f3 <- function(t) 1.5*sin(2*pi*t/4)
-#'f4 <- function(t) 1.5*atan(2*pi*t/4)
+#'f3 <- function(t) 2*sin(2*pi*t/4)
+#'f4 <- function(t) 2*atan(2*pi*t/4)
 #'
-#'n<-1000
+#'n<-100
 #'
 #'sig <- diag(c(4.0,2.0,1.5,1.2))
 #'
@@ -48,9 +49,11 @@
 #'  Lx[[i]] <- scoreX[i,1]*phi1(grid) + scoreX[i,2]*phi2(grid) + scoreX[i,3]*phi3(grid) + scoreX[i,4]*phi4(grid)
 #'}
 #'
-#'N <- 101
-#'xi <- matrix(rep(seq(-2,2,length.out=N),4),nrow=N,ncol=4)
-#'fit <- FAM(Y=Y,Lx=Lx,Lt=Lt,xi=xi)
+#'
+#'# estimation
+#'fit <- FAM(Y=Y,Lx=Lx,Lt=Lt)
+#'
+#'xi <- fit$xi
 #'
 #'par(mfrow=c(2,2))
 #'j <- 1
@@ -77,46 +80,76 @@
 #'plot(sort(xi[,j]),g4,type='l',col=2,ylim=c(-2.5,2.5),xlab='xi4')
 #'points(sort(xi[,j]),tmpSgn*fit$fam[order(xi[,j]),j],type='l')
 #'
+#'
+#'# fitting
+#'fit <- FAM(Y=Y,Lx=Lx,Lt=Lt,nEval=0)
+#'yHat <- fit$mu+apply(fit$fam,1,'sum')
+#'par(mfrow=c(1,1))
+#'plot(yHat,Y)
+#'abline(coef=c(0,1),col=2)
+#'
+#'
+#'# R^2
+#'R2 <- 1-sum((Y-yHat)^2)/sum((Y-mean(Y))^2)
+#'R2
+#'
 #' 
 #' @references
 #' \cite{Mueller, H.-G. and Yao, F. (2005), "Functional additive models", JASA, Vol.103, No.484, p.1534-1544.}
 #' 
 #' @export
 
-FAM <- function(Y,Lx,Lt,K='epan',FVE=NULL,xi=NULL,h=NULL){
+FAM <- function(Y,Lx,Lt,nEval=51,bwMethod=5,supp=c(-2,2),optns=list()){
   
-  N <- 51
   n <- length(Y)
   
-  if (is.null(FVE)==TRUE) {
-    FVE <- 0.95
-  } 
-  
-  tmpFPCA <- FPCA(Lx, Lt, optns = list(FVEthreshold=FVE))
+  XiStd <- c()
+  estLambda <- c()
+
+  tmpFPCA <- FPCA(Lx, Lt)
     
-  Xi <- tmpFPCA$xiEst
+  XiStd <- t(t(tmpFPCA$xiEst)/sqrt(tmpFPCA$lambda))
   d <- length(tmpFPCA$lambda)
   
-  if (is.null(xi)==TRUE) {
-    xi <- matrix(rep(seq(-2,2,length.out=N),d),nrow=N,ncol=d)
+  estLambda <- tmpFPCA$lambda
+  
+  N <- c()
+  xiStdGrid <- c()
+  if (nEval==0) {
+    N <- nrow(XiStd)
+    xiStdGrid <- XiStd
+  } else {
+    N <- nEval
+    xiStdGrid <- matrix(rep(seq(supp[1],supp[2],length.out=N),d),nrow=N,ncol=d)
   }
   
-  if (is.null(h)==TRUE) {
-    h <- c()
-    for (j in 1:d) {
-      options(warn = -1) 
-      h[j] <- fdapace:::CVLwls1D(y=(Y-mean(Y)),t=Xi[,j],kernel='epan',npoly=1,nder=0,dataType='Dense',kFolds=20)/2
-      options(warn = 0) 
-    } 
-  }
-  
-  fam <- matrix(nrow=nrow(xi),ncol=ncol(xi))
+  h <- c()
   for (j in 1:d) {
-    fam[,j] <- fdapace:::Lwls1D(bw=h[j],kernel_type='epan',xin=sort(Xi[,j]),yin=(Y[order(Xi[,j])]-mean(Y)),xout=xi[,j],npoly=1,nder=0)
+    if (bwMethod>0) {
+      options(warn = -1) 
+      h[j] <- fdapace:::CVLwls1D(y=(Y-mean(Y)),t=XiStd[,j],kernel='epan',npoly=1,nder=0,dataType='Sparse',kFolds=bwMethod)
+      options(warn = 0) 
+    } else {
+      options(warn = -1) 
+      h[j] <- fdapace:::GCVLwls1D1(y=(Y-mean(Y)),t=XiStd[,j],kernel='epan',npoly=1,nder=0,dataType='Sparse')$bOpt
+      options(warn = 0) 
+    }
+  } 
+  
+  fam <- matrix(nrow=N,ncol=d)
+  for (j in 1:d) {
+    xiTmp <- sort(xiStdGrid[,j])
+    fitTmp <- fdapace:::Lwls1D(bw=h[j],kernel_type='epan',xin=sort(XiStd[,j]),yin=(Y[order(XiStd[,j])]-mean(Y)),xout=xiTmp,npoly=1,nder=0)
+    fam[,j] <- fitTmp[match(xiStdGrid[,j],xiTmp)]
   }
   yMean <- mean(Y)
   
-  fit <- list(mu=yMean, fam=fam, xi=xi)
+  xiGrid <- xiStdGrid%*%diag(sqrt(estLambda))
+  
+  bw <- h*sqrt(estLambda)
+  
+  fit <- list(xi=xiGrid, mu=yMean, fam=fam, bw=bw)
+  
   return(fit)
   
 }
--- R/MultiFAM.R
+++ R/MultiFAM.R
@@ -3,93 +3,105 @@
 #' Smooth backfitting procedure for functional additive models with multiple predictor processes
 #'
 #' @param Y An \emph{n}-dimensional vector whose elements consist of scalar responses.
-#' @param Xi A \eqn{(K_1 + ... + K_d)}-dimensional list whose components consist of two lists of \emph{n} vectors containing the obervation time and functional predictor values. See \code{FPCA} for detail.
-#' @param K A \code{function} object representing the kernel to be used in the smooth backfitting (default is 'epan', the the Epanechnikov kernel).
-#' @param FVE A \emph{d}-dimesional vector whose components consist of the fraction of variance explained of of FPCA for each predictor process (default is 0.95).
-#' @param xi An \emph{N} by \eqn{(K_1 + ... + K_d)} matrix whose column vectors consist of \emph{N} vectors of estimation points for each component function.
-#' @param h A \eqn{(K_1 + ... + K_d)}-dimensional vector of bandwidths for kernel smoothing to estimate each component function (default is NULL, but it automatically apply shrinkage factor bandwidth selector. See Han et al. (2016)).
-#' @param supp A \eqn{(K_1 + ... + K_d)} by 2 matrix whose row vectors consist of the lower and upper limits of estimation intervals for each component function (default is the \emph{d}-dimensional rectangle of \emph{[-2,2]}).
+#' @param X A \emph{d}-dimensional list whose components consist of two lists of \emph{Ly} and \emph{Lt} containing obervation times and functional covariate values for each predictor component, respectively. For details of \emph{Ly} and \emph{Lt}, see \code{FPCA} for detail.
+#' @param ker A \code{function} object representing the base kernel to be used in the smooth backfitting algorithm (default is 'epan' which is the only option supported currently).
+#' @param nEval The number of evaluation grid points for kernel smoothing (default is 51. If it is specified as 0, then estimated FPC scores are used for evaluation grid instead of equal grid).
+#' @param alpha The shrinkage factor (positive number) for bandwidth selection. See Han et al. (2016) (default is 0.5).
+#' @param supp The lower and upper limits of kernel smoothing domain for studentized FPC scores (default is [-2,2]).
+#' @param optnsList A \emph{d}-dimensional list whose components consist of \emph{optns} for each predictor component, respectively. (default is NULL which assigns the same default \emph{optns} uased in \code{FPCA}).
 #'
 #' @details \code{MultiFAM} fits functional additive models for a scalar response and multiple predictor processes based on the smooth backfitting algorithm proposed by Han et al. (2016) that \deqn{E(Y | \mathbf{X}) = \sum_{j=1}^d \sum_{k=1}^{K_j} g_{jk}(\xi_{jk}),} where \eqn{\xi_{jk}} stand for the k-th FPC score of the j-th predictor process. \code{MultiFAM} only focuses on mutiple predictor processes case. In fact, the case of univariate predictor is the same with functional additive model proposed by Mueller and Yao (2008). Especially in this development, one can designate an estimation support of additive models when the additive modeling is only allowed over restricted intervals or one is interested in the modeling over the support (see Han et al., 2016).
 #'
 #' @return A list containing the following fields:
 #' \item{xi}{An \emph{N} by \eqn{(K_1 + ... + K_d)} matrix whose column vectors consist of FPC score grid vectors that each additive component functional is evluated.}
+#' \item{mu}{A scalar of centered part of the regression model.}
 #' \item{SBFit}{An \emph{N} by \eqn{(K_1 + ... + K_d)} matrix whose column vectors consist of the smooth backfitting component function estimators at the given estimation points.}
-#' \item{mY}{A scalar of centered part of the regression model.}
-#' \item{NW}{An \emph{N} by \eqn{(K_1 + ... + K_d)} matrix whose column vectors consist of the Nadaraya-Watson marginal regression function estimators for each predictor component at the given estimation points.}
+#' \item{bw}{A \eqn{(K_1 + ... + K_d)}-dimensional bandwidth vector.}
 #' @examples
-#' set.seed(1000)
+#'set.seed(1000)
+#'
+#'library(MASS)
+#'
+#'f11 <- function(t) t
+#'f12 <- function(t) 2*cos(2*pi*t/4)
+#'f21 <- function(t) 1.5*sin(2*pi*t/4)
+#'f22 <- function(t) 1.5*atan(2*pi*t/4)
+#'
+#'n<-100
+#'
+#'sig <- matrix(c(2.0, 0.0, 0.5, -.2,
+#'                0.0, 1.2, -.2, 0.3,
+#'                0.5, -.2, 1.7, 0.0,
+#'                -.2, 0.3, 0.0, 1.0),
+#'              nrow=4,ncol=4)
+#'
+#'scoreX <- mvrnorm(n,mu=rep(0,4),Sigma=sig)
+#'Y <- f11(scoreX[,1]) + f12(scoreX[,2]) + f21(scoreX[,3]) + f22(scoreX[,4]) + rnorm(n,0,0.5)
+#'
+#'phi11 <- function(t) sqrt(2)*sin(2*pi*t)
+#'phi12 <- function(t) sqrt(2)*sin(4*pi*t)
+#'phi21 <- function(t) sqrt(2)*cos(2*pi*t)
+#'phi22 <- function(t) sqrt(2)*cos(4*pi*t)
+#'
+#'grid <- seq(0,1,length.out=21)
+#'Lt <- Lx1 <- Lx2 <- list()
+#'for (i in 1:n) {
+#'  Lt[[i]] <- grid
+#'  Lx1[[i]] <- scoreX[i,1]*phi11(grid) + scoreX[i,2]*phi12(grid)
+#'  Lx2[[i]] <- scoreX[i,3]*phi21(grid) + scoreX[i,4]*phi22(grid)
+#'}
+#'
+#'X1 <- list(Ly=Lx1, Lt=Lt)
+#'X2 <- list(Ly=Lx2, Lt=Lt)
+#'
+#'X <- list(X1, X2)
+#'
+#'# estimation
+#'sbf <- MultiFAM(Y=Y,X=X)
+#'
+#'xi <- sbf$xi
+#'
+#'par(mfrow=c(2,2))
+#'j <- 1
+#'p0 <- trapzRcpp(sort(xi[,j]),dnorm(sort(xi[,j]),0,sqrt(sig[j,j])))
+#'g11 <- f11(sort(xi[,j])) - trapzRcpp(sort(xi[,j]),f11(sort(xi[,j]))*dnorm(sort(xi[,j]),0,sqrt(sig[j,j])))/p0
+#'tmpSgn <- sign(sum(g11*sbf$SBFit[,j]))
+#'plot(sort(xi[,j]),g11,type='l',col=2,ylim=c(-2.5,2.5),xlab='xi11')
+#'points(sort(xi[,j]),tmpSgn*sbf$SBFit[order(xi[,j]),j],type='l')
+#'
+#'j <- 2
+#'p0 <- trapzRcpp(sort(xi[,j]),dnorm(sort(xi[,j]),0,sqrt(sig[j,j])))
+#'g12 <- f12(sort(xi[,j])) - trapzRcpp(sort(xi[,j]),f12(sort(xi[,j]))*dnorm(sort(xi[,j]),0,sqrt(sig[j,j])))/p0
+#'tmpSgn <- sign(sum(g12*sbf$SBFit[,j]))
+#'plot(sort(xi[,j]),g12,type='l',col=2,ylim=c(-2.5,2.5),xlab='xi12')
+#'points(sort(xi[,j]),tmpSgn*sbf$SBFit[order(xi[,j]),j],type='l')
+#'
+#'j <- 3
+#'p0 <- trapzRcpp(sort(xi[,j]),dnorm(sort(xi[,j]),0,sqrt(sig[j,j])))
+#'g21 <- f21(sort(xi[,j])) - trapzRcpp(sort(xi[,j]),f21(sort(xi[,j]))*dnorm(sort(xi[,j]),0,sqrt(sig[j,j])))/p0
+#'tmpSgn <- sign(sum(g21*sbf$SBFit[,j]))
+#'plot(sort(xi[,j]),g21,type='l',col=2,ylim=c(-2.5,2.5),xlab='xi21')
+#'points(sort(xi[,j]),tmpSgn*sbf$SBFit[order(xi[,j]),j],type='l')
+#'
+#'j <- 4
+#'p0 <- trapzRcpp(sort(xi[,j]),dnorm(sort(xi[,j]),0,sqrt(sig[j,j])))
+#'g22 <- f22(sort(xi[,j])) - trapzRcpp(sort(xi[,j]),f22(sort(xi[,j]))*dnorm(sort(xi[,j]),0,sqrt(sig[j,j])))/p0
+#'tmpSgn <- sign(sum(g22*sbf$SBFit[,j]))
+#'plot(sort(xi[,j]),g22,type='l',col=2,ylim=c(-2.5,2.5),xlab='xi22')
+#'points(sort(xi[,j]),tmpSgn*sbf$SBFit[order(xi[,j]),j],type='l')
+#'
+#'
+#'# fitting
+#'sbf <- MultiFAM(Y=Y,X=X,nEval=0)
+#'yHat <- sbf$mu+apply(sbf$SBFit,1,'sum')
+#'par(mfrow=c(1,1))
+#'plot(yHat,Y)
+#'abline(coef=c(0,1),col=2)
 #' 
-#' library(MASS)
 #' 
-#' trapzRcpp <- fdapace:::trapzRcpp
-#' 
-#' f11 <- function(t) t
-#' f12 <- function(t) 2*cos(2*pi*t/4)
-#' f21 <- function(t) 1.5*sin(2*pi*t/4)
-#' f22 <- function(t) 1.5*atan(2*pi*t/4)
-#' 
-#' n<-100
-#' 
-#' sig <- matrix(c(2.0, 0.0, 0.5, -.2,
-#'                 0.0, 1.2, -.2, 0.3,
-#'                 0.5, -.2, 1.7, 0.0,
-#'                 -.2, 0.3, 0.0, 1.0),
-#'               nrow=4,ncol=4)
-#' 
-#' scoreX <- mvrnorm(n,mu=rep(0,4),Sigma=sig)
-#' Y <- f11(scoreX[,1]) + f12(scoreX[,2]) + f21(scoreX[,3]) + f22(scoreX[,4]) + rnorm(n,0,0.5)
-#' 
-#' phi11 <- function(t) sqrt(2)*sin(2*pi*t)
-#' phi12 <- function(t) sqrt(2)*sin(4*pi*t)
-#' phi21 <- function(t) sqrt(2)*cos(2*pi*t)
-#' phi22 <- function(t) sqrt(2)*cos(4*pi*t)
-#' 
-#' grid <- seq(0,1,length.out=51)
-#' Lt <- Lx1 <- Lx2 <- list()
-#' for (i in 1:n) {
-#'   Lt[[i]] <- grid
-#'   Lx1[[i]] <- scoreX[i,1]*phi11(grid) + scoreX[i,2]*phi12(grid)
-#'   Lx2[[i]] <- scoreX[i,3]*phi21(grid) + scoreX[i,4]*phi22(grid)
-#' }
-#' 
-#' Xi1 <- list(Ly=Lx1, Lt=Lt)
-#' Xi2 <- list(Ly=Lx2, Lt=Lt)
-#' 
-#' Xi <- list(Xi1, Xi2)
-#' 
-#' xi <- matrix(rep(seq(-2,2,length.out=101),4),nrow=101,ncol=4)
-#' sbf <- MultiFAM(Y=Y,Xi=Xi,xi=xi)
-#' 
-#' par(mfrow=c(2,2))
-#' j <- 1
-#' p0 <- trapzRcpp(sort(xi[,j]),dnorm(sort(xi[,j]),0,sqrt(sig[j,j])))
-#' g11 <- f11(sort(xi[,j])) - trapzRcpp(sort(xi[,j]),f11(sort(xi[,j]))*dnorm(sort(xi[,j]),0,sqrt(sig[j,j])))/p0
-#' tmpSgn <- sign(sum(g11*sbf$SBFit[,j]))
-#' plot(sort(xi[,j]),g11,type='l',col=2,ylim=c(-2.5,2.5),xlab='xi11')
-#' points(sort(xi[,j]),tmpSgn*sbf$SBFit[order(xi[,j]),j],type='l')
-#' 
-#' j <- 2
-#' p0 <- trapzRcpp(sort(xi[,j]),dnorm(sort(xi[,j]),0,sqrt(sig[j,j])))
-#' g12 <- f12(sort(xi[,j])) - trapzRcpp(sort(xi[,j]),f12(sort(xi[,j]))*dnorm(sort(xi[,j]),0,sqrt(sig[j,j])))/p0
-#' tmpSgn <- sign(sum(g12*sbf$SBFit[,j]))
-#' plot(sort(xi[,j]),g12,type='l',col=2,ylim=c(-2.5,2.5),xlab='xi12')
-#' points(sort(xi[,j]),tmpSgn*sbf$SBFit[order(xi[,j]),j],type='l')
-#' 
-#' j <- 3
-#' p0 <- trapzRcpp(sort(xi[,j]),dnorm(sort(xi[,j]),0,sqrt(sig[j,j])))
-#' g21 <- f21(sort(xi[,j])) - trapzRcpp(sort(xi[,j]),f21(sort(xi[,j]))*dnorm(sort(xi[,j]),0,sqrt(sig[j,j])))/p0
-#' tmpSgn <- sign(sum(g21*sbf$SBFit[,j]))
-#' plot(sort(xi[,j]),g21,type='l',col=2,ylim=c(-2.5,2.5),xlab='xi21')
-#' points(sort(xi[,j]),tmpSgn*sbf$SBFit[order(xi[,j]),j],type='l')
-#' 
-#' j <- 4
-#' p0 <- trapzRcpp(sort(xi[,j]),dnorm(sort(xi[,j]),0,sqrt(sig[j,j])))
-#' g22 <- f22(sort(xi[,j])) - trapzRcpp(sort(xi[,j]),f22(sort(xi[,j]))*dnorm(sort(xi[,j]),0,sqrt(sig[j,j])))/p0
-#' tmpSgn <- sign(sum(g22*sbf$SBFit[,j]))
-#' plot(sort(xi[,j]),g22,type='l',col=2,ylim=c(-2.5,2.5),xlab='xi22')
-#' points(sort(xi[,j]),tmpSgn*sbf$SBFit[order(xi[,j]),j],type='l')
-#' 
+#'# R^2
+#'R2 <- 1-sum((Y-yHat)^2)/sum((Y-mean(Y))^2)
+#'R2
 #' 
 #' @references
 #' \cite{Mammen, E., Linton, O. and Nielsen, J. (1999), "The existence and asymptotic properties of a backfitting projection algorithm under weak conditions", Annals of Statistics, Vol.27, No.5, p.1443-1490.}
@@ -101,93 +113,68 @@
 #' \cite{Han, K., Mueller, H.-G. and Park, B. U. (2016), "Smooth backfitting for additive modeling with small errors-in-variables, with an application to additive functional regression for multiple predictor functions", Bernoulli (accepted).}
 #' @export
 
-MultiFAM <- function(Y,Xi,K='epan',FVE=NULL,xi=NULL,h=NULL,supp=NULL){
+MultiFAM <- function(Y,X,ker='epan',nEval=51,alpha=0.5,supp=c(-2,2),optnsList=NULL){
   
-  N <- 51
   n <- length(Y)
-  d0 <- length(Xi)
+  d0 <- length(X)
   
-  if (is.null(FVE)==TRUE) {
-    FVE <- rep(0.95,d0)
-  } else {
-    if (length(FVE)<2) {
-      FVE <- rep(FVE,d0)
+  if (is.null(optnsList)==TRUE) {
+    for (j in 1:d0) {
+      optnsList[[j]] <- list() 
     }
   }
-  
-  supp00 <- matrix(rep(c(-2,2),d0),ncol=2,byrow=TRUE)
-  
-  x0 <- c()
   
   dj <- c()
-  X <- c()
-  supp0 <- matrix(ncol=2)
+  XiStd <- c()
+  estLambda <- c()
   for (j in 1:d0) {
-    tmpLy <- Xi[[j]]$Ly
-    tmpLt <- Xi[[j]]$Lt
+    tmpLy <- X[[j]]$Ly
+    tmpLt <- X[[j]]$Lt
     
-    tmpFPCA <- FPCA(tmpLy, tmpLt, optns = list(FVEthreshold=FVE[j]))
+    tmpFPCA <- FPCA(tmpLy, tmpLt, optns = optnsList[[j]])
     
-    Xij <- t(t(tmpFPCA$xiEst)/sqrt(tmpFPCA$lambda))
-    dj <- length(tmpFPCA$lambda)
+    XijStd <- t(t(tmpFPCA$xiEst)/sqrt(tmpFPCA$lambda))
+    dj[j] <- length(tmpFPCA$lambda)
     
-    X <- cbind(X,Xij)
+    estLambda <- c(estLambda,tmpFPCA$lambda)
     
-    xj0 <- seq(supp00[j,1],supp00[j,2],length.out=N)
-    x0 <- cbind(x0, matrix(rep(xj0,dj),ncol=dj))
-    
-    
-    supp0 <- t(cbind(t(supp0),matrix(rep(supp00[j,],dj),ncol=2)))
+    XiStd <- cbind(XiStd,XijStd)
   }
+  d <- sum(dj)
   
-  supp0 <- supp0[-1,]
   
-  if (is.null(supp)==TRUE) {
-    supp <- supp0
-  }
-  
-  if (ncol(xi)!=ncol(X)) {
-    stop('Column length of evaluation grid matrix should have the same with K1+...+Kd')
-  }
-  
-  if (is.null(xi)==TRUE) {
-    x <- x0
+  N <- c()
+  xiStdGrid <- c()
+  if (nEval==0) {
+    N <- nrow(XiStd)
+    xiStdGrid <- XiStd
   } else {
-    x <- xi
-    
-    tmpIndex <- rep(1,nrow(x))
-    for (l in 1:ncol(x)) {
-      tmpIndex <- tmpIndex*dunif(x[,l],supp[l,1],supp[l,2])*(supp[l,2]-supp[l,1])
-    }
-    tmpIndex <- which(tmpIndex==1)
-    
-    x <- x[tmpIndex,]
-    
+    N <- nEval
+    xiStdGrid <- matrix(rep(seq(supp[1],supp[2],length.out=N),d),nrow=N,ncol=d)
   }
   
-  d <- ncol(X)
+  if (ker!='epan') {
+    stop('Epanechnikov kernel is only supported currently. It uses Epanechnikov kernel automatically')
+  } 
+  K<-'epan'
   
-  if (K!='epan') {
-    message('Epanechnikov kernel is only supported currently. It uses Epanechnikov kernel automatically')
-    K<-'epan'
-  }
-  if (is.null(h)==TRUE) {
-    # h <- rep(0.4*n^(-1/5),d)*(supp[,2]-supp[,1])
-    h <- c()
-    for (j in 1:d) {
-      options(warn = -1) 
-      h0 <- n^(-1/5)*fdapace:::GCVLwls1D1(y=Y,t=X[,j],kernel='epan',npoly=0,nder=0,dataType='Dense')$bOpt
-      options(warn = 0) 
-      while (h0 > (supp[j,2]-supp[j,1])/5) {
-        h0 <- (supp[j,2]-supp[j,1])/6
-      }
-      h[j] <- h0
-    }
-  } else {
-    h <- rep(h,dj)
+  h <- c()
+  for (j in 1:d) {
+    options(warn = -1) 
+    h0 <- fdapace:::GCVLwls1D1(y=Y,t=XiStd[,j],kernel='epan',npoly=0,nder=0,dataType='Sparse')$bOpt
+    options(warn = 0) 
+    h[j] <- alpha*h0
   }
   
-  sbf <- SBFitting(Y,x,X,h,supp=supp)
+  supp <- matrix(rep(supp,c(d,d)),nrow=d,ncol=2)
+
+  sbf <- SBFitting(Y,xiStdGrid,XiStd,h,supp=supp)
+  
+  xiGrid <- xiStdGrid%*%diag(sqrt(estLambda))
+  
+  bw <- h*sqrt(estLambda)
+  
+  sbf <- list(xi=xiGrid,mu=sbf$mY,SBFit=sbf$SBFit,bw=bw)
   
   return(sbf)
   
