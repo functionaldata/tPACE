84a6aadf0bb84ab9ff3096377290ad57230041c8
Yaqing Chen <ychen@stat-mbp-ychen.attlocal.net>
2019-05-30 14:05:32 -0700

    Revert "Merge pull request #47 from functionaldata/vcam-merge"
    This reverts commit 1a4f78a8f540e75314d8d1473feeb1171a5bfcd7, reversing
    changes made to 001a8d0fb5ae5fc11e28f510052376c1383c522f.

--- fdapace.Rcheck/vign_test/fdapace/tests/AAAtestthat.R
+++
@@ -1,9 +0,0 @@
-# # Based on https://github.com/hadley/testthat#integration-with-r-cmd-check
-# splitting test files into multiple ones so that each one runs within 10 mins,
-# which is the limit on travis CI.
-# This file contains the "fast" checks.
-
-library(testthat)
-library(fdapace)
-test_check("fdapace", filter='_(?!FPCA)(?!FCReg)(?!FClust)(?!FSVD)(?!FVPA)(?!MultiFAM)', perl=TRUE)
-# test_check("fdapace")
