11e79a164ad56ff6e66dd980e3214e113d334d75
Cody Carroll <cjcarroll@ucdavis.edu>
2019-07-22 12:55:05 -0700

    lose check files

--- fdapace.Rcheck/00_pkg_src/fdapace/tests/testthat_slow.R
+++
@@ -1,9 +0,0 @@
-# # Based on https://github.com/hadley/testthat#integration-with-r-cmd-check
-
-# Splitting test files into multiple ones so that each one runs within 10 mins,
-# which is the limit on travis CI.
-# This file contains the slow running tests.
-
-library(testthat)
-library(fdapace)
-test_check("fdapace", filter='FClust|FSVD|FPCA|FVPA|FCReg|FOptDes|GetCrCovYX|selectK|WFDA|MultiFAM|', perl=TRUE) #
