cfd697a1ed59b8b84d32bafedc682838bd61ae96
Cody Carroll <cjcarroll@ucdavis.edu>
2020-06-16 19:03:36 -0700

    adjust tests to account for new isRegular dense check

--- tests/testthat/test_IsRegular.R
+++ tests/testthat/test_IsRegular.R
@@ -1,7 +1,7 @@
 cat("\ntests for 'IsRegular'")
 
 test_that("basic valid lists arguments do not return any errors", { 
-  expect_equal(IsRegular(list(c(1,2,3,4), c(1,2,3,4), c(1,2,3,4))), 'Dense') 
+  expect_equal(IsRegular(list(c(1:10), c(1:10), c(1:10))), 'Dense') 
   expect_equal(IsRegular(list(c(1,2,3  ), c(1,2,3,4), c(1,2,3,4))), 'DenseWithMV') 
   expect_equal(IsRegular(list(c(1,2   ), c(1,2,3  ), c(1,2,3,4))), 'Sparse') 
 }
--- tests/testthat/test_SetOptions.R
+++ tests/testthat/test_SetOptions.R
@@ -7,7 +7,7 @@ library(testthat)
  
  test_that("SetOptions test: optns$method = NULL 'IN' for Dense case, 'CE' for Sparse case, 'CE' for other cases with warning", { 
   expect_equal(SetOptions(list(c(1,3,5), c(2,4)),list(c(1,3,5), c(2,4)), optns)$methodXi, 'CE') 
-   expect_equal(SetOptions(list(c(1,2,3), c(1,2,3)),list(c(1,2,3), c(1,2,3)), optns)$methodXi, 'IN') 
-  expect_equal(SetOptions(list(c(1,2,3), c(1,2,3)),list(c(1,2,3), c(1,2,3)), optns)$kernel, 'epan') 
+   expect_equal(SetOptions(list(c(1:10), c(1:10)),list(c(1:10), c(1:10)), optns)$methodXi, 'IN') 
+  expect_equal(SetOptions(list(c(1:10), c(1:10)),list(c(1:10), c(1:10)), optns)$kernel, 'epan') 
   expect_equal(SetOptions(list(c(1,2,3,4,5), c(1,2,3,4)),list(c(1,2,3,4,5), c(1,2,3,4)), optns)$methodXi, 'CE') # DenseWithMV so should be 'IN' not 'CE'?
 })
