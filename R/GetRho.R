76adcbb1c262ccbea536a4382c3d48c9cba48f04
Alvaro E. Gajardo <aegajardo@ucdavis.edu>
2020-11-13 21:48:36 -0800

    removes loop getRho

--- R/GetRho.R
+++ R/GetRho.R
@@ -2,10 +2,6 @@ GetRho <- function(y, t, optns, mu,muwork, obsGrid, fittedCov, lambda, phi, phiw
   
   optnsTmp <- optns
   optnsTmp$verbose <- FALSE
-  for (j in 1:2) {
-    yhat <- GetCEScores(y, t, optnsTmp, mu, obsGrid, fittedCov, lambda, phi, sigma2)[3, ] 
-    sigma2 <- mean(mapply(function(a, b) mean((a - b)^2, na.rm=TRUE), yhat, y), na.rm=TRUE)
-  }
   ls = sapply(t,length)
   y = y[ls >0]
   t= t[ls > 0]
